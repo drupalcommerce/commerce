d90d7d18a8df6621ababacf6a7e78237179c17b2
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2010-07-12 13:17:13 +0000

    First steps of the payment implementation (still very messy and non-functional).

---
+++ modules/payment/commerce_payment.api.php
@@ -0,0 +1,17 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Documentation for Drupal Commerce Payment API.
+ */
+
+/**
+ * Define payment methods.
+ */
+function hook_commerce_payment_method_info() {
+  $method['paypal'] = array(
+    
+  );
+  return $method;
+}
