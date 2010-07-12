e2917104f0044a147c14dd2e9c83c316536c9e67
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2010-07-12 13:17:35 +0000

    #799472 disable the checkout continue button and provide visual notification to the user that its processing.

---
+++ modules/checkout/commerce_checkout.js
@@ -0,0 +1,26 @@
+// $Id$
+;(function($) {
+
+  /**
+   * Disable the continue buttons in the checkout process once they are clicked
+   * and provide a notification to the user.
+   */
+  Drupal.behaviors.commerceCheckout = {
+    attach: function (context, settings) {
+      // When the buttons to move from page to page in the checkout process are
+      // clicked we disable them so they are not accidently clicked twice.
+      $('input.checkout-continue', context).click(function() {
+        $this = $(this);
+        $this.clone().insertAfter(this).attr('disabled', true).after(Drupal.theme('checkoutProcessing'));
+        $this.hide();
+      });
+    }
+  }
+
+  /**
+   * Theme function for checkout button clicked processing indication.
+   */
+  Drupal.theme.prototype.checkoutProcessing = function() {
+    return '<span class="checkout-processing"></span>';
+  }
+})(jQuery);
