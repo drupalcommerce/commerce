5a2960d6afd497d0a0d21326fcaed78826258296
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2010-08-12 01:20:54 +0000

    #842122 by rszrama: add an area handler for summarizing the contents of a line item View; used on the shopping cart block View.

--- modules/cart/theme/commerce-cart-block.tpl.php
+++ modules/cart/theme/commerce-cart-block.tpl.php
@@ -7,14 +7,6 @@
  *
  * Available variables:
  * - $contents_view: A rendered View containing the contents of the cart.
- * - $quantity_raw: The number of items in the cart.
- * - $quantity_label: The quantity appropriate label to use for the number of
- *   items in the shopping cart; "item" or "items" by default.
- * - $quantity: A single string containing the number and label.
- * - $total_raw: The raw numeric value of the total value of items in the cart.
- * - $total_label: A text label for the total value; "Total:" by default.
- * - $total: The currency formatted total value of items in the cart.
- * - $cart_links: A rendered links array with cart and checkout links.
  *
  * Helper variables:
  * - $order: The full order object for the shopping cart.
@@ -25,13 +17,4 @@
 ?>
 <div class="cart-contents">
   <?php print $contents_view; ?>
-</div>
-<div class="cart-footer">
-  <div class="cart-quantity">
-    <span class="cart-quantity-raw"><?php print $quantity_raw; ?></span> <span class="cart-quantity-label"><?php print $quantity_label; ?></span>
-  </div>
-  <div class="cart-total">
-    <span class="cart-total-label"><?php print $total_label; ?></span> <span class="cart-total-raw"><?php print $total; ?></span>
-  </div>
-  <?php print $cart_links; ?>
 </div>
---
+++ modules/line_item/theme/commerce-line-item-summary.tpl.php
@@ -0,0 +1,33 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Default implementation of a line item summary template.
+ *
+ * Available variables:
+ * - $quantity_raw: The number of items in the cart.
+ * - $quantity_label: The quantity appropriate label to use for the number of
+ *   items in the shopping cart; "item" or "items" by default.
+ * - $quantity: A single string containing the number and label.
+ * - $total_raw: The raw numeric value of the total value of items in the cart.
+ * - $total_label: A text label for the total value; "Total:" by default.
+ * - $total: The currency formatted total value of items in the cart.
+ * - $links: A rendered links array with cart and checkout links.
+ *
+ * Helper variables:
+ * - $view: The View the line item summary is attached to.
+ *
+ * @see template_preprocess()
+ * @see template_process()
+ */
+?>
+<div class="line-item-summary">
+  <div class="line-item-quantity">
+    <span class="line-item-quantity-raw"><?php print $quantity_raw; ?></span> <span class="line-item-quantity-label"><?php print $quantity_label; ?></span>
+  </div>
+  <div class="line-item-total">
+    <span class="line-item-total-label"><?php print $total_label; ?></span> <span class="line-item-total-raw"><?php print $total; ?></span>
+  </div>
+  <?php print $links; ?>
+</div>
