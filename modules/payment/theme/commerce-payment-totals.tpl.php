0baf8172f9576b228731734ec8d0560e663ef759
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2010-09-28 05:26:40 +0000

    Provided Views integration for all payment transaction fields including an updated area handler to add totals and order balance information to a payment transaction View.

---
+++ modules/payment/theme/commerce-payment-totals.tpl.php
@@ -0,0 +1,26 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Default implementation of a payment totals template.
+ *
+ * Available variables:
+ * - $rows: A rows array as used by theme_table(), potentially containing
+ *     transaction totals, an order balance, and information specified by
+ *     other modules.
+ * - $form: When present, a form for adding payments to an order pertinent to
+ *     the display.
+ *
+ * Helper variables:
+ * - $totals: An array of transaction totals keyed by currency code.
+ * - $view: The View the line item summary is attached to.
+ *
+ * @see template_preprocess()
+ * @see template_process()
+ */
+?>
+<div class="payment-totals">
+  <?php print theme('table', array('rows' => $rows, 'attributes' => array('class' => array('payment-totals-table')))); ?>
+  <?php print $form; ?>
+</div>
