6c0f984f70093d6f7eb82ca9c5ebe8945fb9fe64
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2010-10-13 22:00:57 +0000

    Add order status, comment, notification, and e-mail fields; shuffle around the fieldset contents, titles, and descriptions.

--- modules/order/commerce_order.js
+++ modules/order/commerce_order.js
@@ -4,18 +4,28 @@
 
 Drupal.behaviors.orderFieldsetSummaries = {
   attach: function (context) {
-    $('fieldset#edit-update-notes', context).drupalSetSummary(function (context) {
+    $('fieldset#edit-order-status', context).drupalSetSummary(function (context) {
+      // If the status has been changed, indicate the original status.
+      if ($('#edit-status').val() != $('#edit-status-original').val()) {
+        return Drupal.t('From @title', { '@title' : Drupal.settings.status_titles[$('#edit-status-original').val()] }) + '<br />' + Drupal.t('To @title', { '@title' : Drupal.settings.status_titles[$('#edit-status').val()] });
+      }
+      else {
+        return Drupal.settings.status_titles[$('#edit-status').val()];
+      }
+    });
+
+    $('fieldset#edit-user', context).drupalSetSummary(function (context) {
+      var name = $('#edit-name').val() || Drupal.settings.anonymous,
+        mail = $('#edit-mail').val();
+      return mail ?
+        Drupal.t('Owned by @name', { '@name' : name }) + '<br />' + mail :
+        Drupal.t('Owned by @name', { '@name': name });
+    });
+
+    $('fieldset#edit-order-log', context).drupalSetSummary(function (context) {
       return $('#edit-log', context).val() ?
         Drupal.t('Notes added') :
         Drupal.t('No notes added');
-    });
-
-    $('fieldset#edit-creation', context).drupalSetSummary(function (context) {
-      var name = $('#edit-name').val() || Drupal.settings.anonymous,
-        date = $('#edit-date').val();
-      return date ?
-        Drupal.t('Owned by @name', { '@name' : name }) + '<br />' + Drupal.t('Created @date', { '@date': date }) :
-        Drupal.t('Owned by @name', { '@name': name });
     });
   }
 };
