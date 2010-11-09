bd96c2ed85dbabcd9a7ededd488369168b9f5b75
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2010-11-09 22:20:13 +0000

    #920836 by rszrama: add hook_commerce_payment_totals_row_info() to define rows for use in the table of the payment totals Views area handler.

--- modules/payment/commerce_payment.api.php
+++ modules/payment/commerce_payment.api.php
@@ -3,8 +3,78 @@
 
 /**
  * @file
- * Documentation for Drupal Commerce Payment API.
+ * Hooks provided by the Payment module.
  */
+
+
+/**
+ * Defines rows for use in payment totals area handlers on Views.
+ *
+ * The payment totals area handler totals the amount of payments received by
+ * currency for all the payment transactions in a View. The array of totals are
+ * is used to build a table containing rows for each of the totals and/or the
+ * remaining balance of the order by default. Other modules may use this hook to
+ * add additional rows to the table.
+ *
+ * @param $totals
+ *   An array of payment totals whose keys are currency codes and values are the
+ *   total amount paid in each currency.
+ * @param $order
+ *   If available, the order object to which the payments apply.
+ *
+ * @return
+ *   An array of table row data as expected by theme_table(). Row arrays may
+ *   contain an additional weight key with the value being an integer used to
+ *   sort the rows prior to display.
+ *
+ * @see commerce_payment_commerce_payment_totals_rows()
+ * @see commerce_payment_commerce_payment_totals_row_info()
+ * @see theme_table()
+ */
+function hook_commerce_payment_totals_row_info($totals, $order) {
+  $rows = array();
+
+  if (count($totals) <= 1) {
+    // Add a row for the remaining balance on the order.
+    if ($order) {
+      $balance = commerce_payment_order_balance($order, $totals);
+
+      $rows[] = array(
+        'data' => array(
+          array('data' => t('Order balance'), 'class' => array('label')),
+          array('data' => commerce_currency_format($balance['amount'], $balance['currency_code']), 'class' => array('balance')),
+        ),
+        'class' => array('order-balance'),
+        'weight' => 10,
+      );
+    }
+  }
+
+  return $rows;
+}
+
+/**
+ * Allows you to alter payment totals rows.
+ *
+ * @param $rows
+ *   Array of payment totals rows exposed by
+ *   hook_commerce_payment_totals_row_info() implementations.
+ * @param $totals
+ *   An array of payment totals whose keys are currency codes and values are the
+ *   total amount paid in each currency.
+ * @param $order
+ *   If available, the order object to which the payments apply.
+ *
+ * @see hook_commerce_payment_totals_row_info()
+ */
+function hook_commerce_payment_totals_row_info_alter(&$rows, $totals, $order) {
+  // Alter the weight of order balance rows to appear first.
+  foreach ($rows as $key => &$row) {
+    if (in_array('order-balance', $row['class'])) {
+      $row['weight'] = -10;
+    }
+  }
+}
 
 /**
  * Define payment methods.
