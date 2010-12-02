06cb21f32d7ab26de63d0985d687cb09c15fbf01
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2010-12-02 06:27:41 +0000

    Remove some outdated, incorrect hook documentation.

--- modules/payment/commerce_payment.api.php
+++ modules/payment/commerce_payment.api.php
@@ -77,39 +77,6 @@ function hook_commerce_payment_totals_row_info_alter(&$rows, $totals, $order) {
 }
 
 /**
- * Define payment methods.
- *
- * Available keys:
- *  - (mandatory) title: the title of the payment method, as displayed in
- *    administrative screens.
- *  - (mandatory) description: the description of the payment method.
- *  - (optional) default settings: an associative array of default settings
- *    for this payment method.
- *  - (optional) base: the base callback for this payment method. The default
- *    is the key of the payment method.
- *  - (optional) callbacks: an associative array of callback functions.
- *    The default for each key is $base . '_' . $callback. Available keys:
- *      - settings: a callback for the settings form
- *      - submit_form: the form displayed during the checkout
- *      - submit_form_validate: the validation callback for the pane form
- *      - submit_form_submit: the submit callback for the pane form
- *      - redirect_form: the form displayed during (optional) redirection
- *      - redirect_form_validate: the validation callback for the redirect form
- *      - redirect_form_submit: the validation callback for the redirect form
- */
-function hook_commerce_payment_method_info() {
-  $method['shiny_gateway'] = array(
-    'title' => t('Shiny gateway'),
-    'description' => t('Secure alliance payment via the Shiny gateway.'),
-    'default settings' => array(
-      'owner name' => 'Malcolm Reynolds',
-      'class code' => '03-K64',
-    ),
-  );
-  return $method;
-}
-
-/**
  * Populates an order's data array with payment methods available in checkout.
  *
  * The Payment module primarily depends on Rules to populate the payment method
