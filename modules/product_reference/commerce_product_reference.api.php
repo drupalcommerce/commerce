06cd45121fabeacd39f131d08cd73f06b2a31155
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2011-01-10 14:53:58 +0000

    Add hooks to the product sell price pre-calculation process to let other modules invalidate particular rule configurations or products.

---
+++ modules/product_reference/commerce_product_reference.api.php
@@ -0,0 +1,63 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Hooks provided by the Product Reference module.
+ */
+
+
+/**
+ * Lets modules invalidate a particular product during the sell price pre-
+ *   calculation process.
+ *
+ * Because the price table can very quickly accumulate millions of rows on
+ * complex websites, it is advantageous to prevent any unnecessary products from
+ * cluttering up the table. This hook allows modules to prevent pre-calculation
+ * on an individual product, which is especially useful when it is known that
+ * products meeting certain criteria will never be featured in Views and other
+ * displays where it might be sorted or filtered based on a calculated price.
+ *
+ * @param $product
+ *   The product being considered for sell price pre-calculation.
+ *
+ * @return
+ *   TRUE or FALSE indicating whether or not the product is valid.
+ *
+ * @see hook_commerce_product_valid_pre_calculation_rule()
+ */
+function hook_commerce_product_valid_pre_calculation_product($product) {
+  // Disable sell price pre-calculation for inactive products.
+  if (!$product->status) {
+    return FALSE;
+  }
+}
+
+/**
+ * Lets modules invalidate a particular rule configuration during the sell price
+ *   pre-calculation process.
+ *
+ * Because the price table can very quickly accumulate millions of rows on
+ * complex websites, it is advantageous to prevent any unnecessary rule
+ * configurations from the pre-calculation process. Each additional rule
+ * configuration exponentially increases the amount of rows necessary for each
+ * product whose sell price is pre-calculated.
+ *
+ * This hook allows modules to prevent pre-calculation for individual rule
+ * configurations, which is especially useful when it is known that certain
+ * rule configurations will never affect the prices of products featured in
+ * Views or other displays that sort or filter based on a calculated price.
+ *
+ * @param $rule
+ *   A rule configuration belonging to the commerce_product_calculate_sell_price
+ *     event.
+ *
+ * @return
+ *   TRUE or FALSE indicating whether or not the rule configuration is valid.
+ *
+ * @see hook_commerce_product_valid_pre_calculation_product()
+ */
+function hook_commerce_product_valid_pre_calculation_rule($rule) {
+  // TODO: Use the implementation specced in http://drupal.org/node/1020976 as
+  // an example here.
+}
