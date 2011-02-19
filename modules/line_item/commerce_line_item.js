17c6e8523499f442ef2a77e6950908a2742d4ea8
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2011-02-19 08:25:50 +0000

    Adding jQuery behavior for preventing users from removing items from the cart when hitting enter on quantity edit text field.

---
+++ modules/line_item/commerce_line_item.js
@@ -0,0 +1,22 @@
+// $Id$
+
+(function($) {
+
+  /**
+   * Trigger the Update button when hitting the enter key.
+   */
+  Drupal.behaviors.commerceLineItemForm = {
+    attach: function (context, settings) {
+      // Click the update button, not the remove button on enter key if we are
+      // on a text field.
+      $('div.commerce-line-item-views-form > form input.form-text', context).keydown(function(event) {
+        if (event.keyCode === 13) {
+          // Prevent browser's default submit from being clicked.
+          event.preventDefault();
+          $('input#edit-update', $(this).parents('form')).click();
+        }
+      });
+    }
+  }
+
+})(jQuery);
