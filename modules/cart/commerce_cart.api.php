e0e63a870375722fd570e519e2d7cd22f4ea47e0
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2011-02-19 08:27:12 +0000

    #797554 by rszrama: create API functions for converting (anonymous -> authenticated) and refreshing (on load) shopping cart orders and use them in hook_user_login() and hook_commerce_order_load().

--- modules/cart/commerce_cart.api.php
+++ modules/cart/commerce_cart.api.php
@@ -33,3 +33,50 @@
 function hook_commerce_cart_order_is_cart($order, $is_cart) {
   // No example.
 }
+
+/**
+ * Allows modules to perform additional processing to convert an anonymous
+ *   shopping cart order to an authenticated cart.
+ *
+ * When anonymous users login to the site, if they have shopping cart orders,
+ * those are converted to authenticated shopping carts. This means their uid and
+ * mail properties are updated along with the uid of any referenced customer
+ * profiles. Additional modules can implement their own logic via this hook,
+ * such as canceling any existing shopping cart orders the user might already
+ * have prior to conversion of the anonymous cart.
+ *
+ * Modules that implement this hook do not need to save changes to the order, as
+ * the Cart module will save the order after invoking the hook.
+ *
+ * @param $order_wrapper
+ *   The entity metadata wrapper for the order being refreshed.
+ * @param $account
+ *   The user account the order will belong to.
+ *
+ * @see commerce_cart_order_convert()
+ */
+function hook_commerce_cart_order_convert($order_wrapper, $account) {
+  // No example.
+}
+
+/**
+ * Allows modules to perform additional processing to refresh a shopping cart
+ *   order's contents.
+ *
+ * When an order is loaded, if it is in a shopping cart order status, its
+ * contents are refreshed to get the current product prices. This prevents users
+ * from checking out orders with stale contents. The API function
+ * commerce_cart_order_refresh() takes care of product line item updates, but
+ * this hook can be used for any additional updates.
+ *
+ * Modules that implement this hook do not need to save changes to the order, as
+ * the Cart module will save the order after invoking the hook.
+ *
+ * @param $order_wrapper
+ *   The entity metadata wrapper for the order being refreshed.
+ *
+ * @see commerce_cart_order_refresh()
+ */
+function hook_commerce_cart_order_refresh($order_wrapper) {
+  // No example.
+}
