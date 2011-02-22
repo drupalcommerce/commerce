ceef2a0f1ccf6875e70c618170d6b60216ade50b
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2011-02-22 20:50:20 +0000

    Add an API function to calculate the total amount of price components and use that in the order controller for calculating the order total; also adds a display formatter for a price field presenting a table of its various components.

---
+++ modules/price/commerce_price.api.php
@@ -0,0 +1,23 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Hooks provided by the Price module.
+ */
+
+
+/**
+ * Lets modules alter price components prior to display through the "Formatted
+ *   amount with components" display formatter.
+ *
+ * @param &$components
+ *   The array of totaled price components.
+ * @param $price
+ *   The price array the components came from.
+ *
+ * @see commerce_price_field_formatter_view()
+ */
+function hook_commerce_price_formatted_components(&$components, $price) {
+  // No example.
+}
