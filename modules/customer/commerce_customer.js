def9223b43006f82ff7196c178eddc35a4dd9858
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2011-02-24 04:04:37 +0000

    Add support for updating the user of a customer profile to the edit form; use vertical tabs for that and the status.

---
+++ modules/customer/commerce_customer.js
@@ -0,0 +1,21 @@
+// $Id$
+
+(function ($) {
+
+Drupal.behaviors.customerFieldsetSummaries = {
+  attach: function (context) {
+    $('fieldset#edit-user', context).drupalSetSummary(function (context) {
+      var name = $('#edit-name').val() || Drupal.settings.anonymous;
+
+      return Drupal.t('Owned by @name', { '@name': name });
+    });
+
+    $('fieldset#edit-profile-status', context).drupalSetSummary(function (context) {
+      return ($('input[@name=status]:checked').val() == 0) ?
+        Drupal.t('Disabled') :
+        Drupal.t('Active');
+    });
+  }
+};
+
+})(jQuery);
