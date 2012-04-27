4766078f269f036188e133ead453f6837e13ac0c
amateescu <amateescu@729614.no-reply.drupal.org>
2012-04-27 18:24:46 -0400

    Issue #1458866 by amateescu: fix the checkout submit throbber on all browsers by adding the span in the form itself and only removing an element-invisible class from it when the checkout button is clicked; minor API change removes the JS theme function Drupal.theme.prototype.checkoutProcessing.

--- modules/checkout/commerce_checkout.js
+++ modules/checkout/commerce_checkout.js
@@ -10,16 +10,10 @@
       // clicked we disable them so they are not accidently clicked twice.
       $('input.checkout-continue:not(.checkout-processed)', context).addClass('checkout-processed').click(function() {
         var $this = $(this);
-        $this.clone().insertAfter(this).attr('disabled', true).after(Drupal.theme('checkoutProcessing'));
+        $this.clone().insertAfter(this).attr('disabled', true).next().removeClass('element-invisible');
         $this.hide();
       });
     }
   }
 
-  /**
-   * Theme function for checkout button clicked processing indication.
-   */
-  Drupal.theme.prototype.checkoutProcessing = function() {
-    return '<span class="checkout-processing"></span>';
-  }
 })(jQuery);
