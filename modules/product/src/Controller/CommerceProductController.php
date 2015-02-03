b96265987ed69d1eb3997f3d9850581a0deaee49
Bojan Zivanovic <bojanz@gmail.com>
2015-02-03 16:17:19 +0100

    Issue #2297117: Introduce a generic AddController.

--- modules/product/src/Controller/CommerceProductController.php
+++ modules/product/src/Controller/CommerceProductController.php
@@ -8,122 +8,12 @@
 namespace Drupal\commerce_product\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Database\Connection;
-use Drupal\Core\Datetime\DateFormatter;
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\commerce_product\CommerceProductTypeInterface;
 use Drupal\commerce_product\CommerceProductInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Returns responses for Commerce Product routes.
  */
-class CommerceProductController extends ControllerBase implements ContainerInjectionInterface {
-
-  /**
-   * The database connection.
-   *
-   * @var \Drupal\Core\Database\Connection
-   */
-  protected $database;
-
-  /**
-   * The date service.
-   *
-   * @var \Drupal\Core\Datetime\DateFormatter
-   */
-  protected $dateFormatter;
-
-  /**
-   * Constructs a CommerceProductController object.
-   *
-   * @param \Drupal\Core\Database\Connection $database
-   *   The database connection.
-   * @param \Drupal\Core\Datetime\DateFormatter $dateFormatter
-   *   The date service.
-   */
-  public function __construct(Connection $database, DateFormatter $dateFormatter) {
-    $this->database = $database;
-    $this->dateFormatter = $dateFormatter;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('database'),
-      $container->get('date.formatter')
-    );
-  }
-
-
-  /**
-   * Displays add content links for available product types.
-   *
-   * Redirects to admin/commerce/products/add/{product-type} if only one product
-   * type is available.
-   *
-   * @return array
-   *   A render array for a list of the product types that can be added; however,
-   *   if there is only one product type defined for the site, the function
-   *   redirects to the product add page for that one product type and does not
-   *   return at all.
-   */
-  public function addPage() {
-    $productTypes = $this->entityManager()->getStorage('commerce_product_type')->loadMultiple();
-    // Filter out the product types the user doesn't have access to.
-    foreach ($productTypes as $productTypeId => $productType) {
-      if (!$this->entityManager()->getAccessControlHandler('commerce_product')->createAccess($productTypeId)) {
-        unset($productTypes[$productTypeId]);
-      }
-    }
-
-    if (count($productTypes) == 1) {
-      $productType = reset($productTypes);
-      return $this->redirect('entity.commerce_product.add_form', array('commerce_product_type' => $productType->id()));
-    }
-
-    return array(
-      '#theme' => 'commerce_product_add_list',
-      '#content' => $productTypes,
-    );
-  }
-
-  /**
-   * Provides the product add form.
-   *
-   * @param \Drupal\commerce_product\CommerceProductTypeInterface $commerce_product_type
-   *   The product type entity for the product.
-   *
-   * @return array
-   *   A product add form.
-   */
-  public function add(CommerceProductTypeInterface $commerce_product_type) {
-    $langCode = $this->moduleHandler()->invoke('language', 'get_default_langcode', array('commerce_product', $commerce_product_type->id()));
-
-    $product = $this->entityManager()->getStorage('commerce_product')->create(array(
-      'type' => $commerce_product_type->id(),
-      'langcode' => $langCode ? $langCode : $this->languageManager()->getCurrentLanguage()->getId(),
-    ));
-
-    $form = $this->entityFormBuilder()->getForm($product, 'add');
-
-    return $form;
-  }
-
-  /**
-   * The _title_callback for the entity.commerce_product.add_form route.
-   *
-   * @param \Drupal\commerce_product\CommerceProductTypeInterface $commerce_product_type
-   *   The current product.
-   *
-   * @return string
-   *   The page title.
-   */
-  public function addPageTitle(CommerceProductTypeInterface $commerce_product_type) {
-    return $this->t('Create @label', array('@label' => $commerce_product_type->label()));
-  }
+class CommerceProductController extends ControllerBase {
 
   /**
    * The _title_callback for the entity.commerce_product.edit_form route
---
+++ src/Controller/AddController.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\commerce\Controller\AddController.
+ */
+
+namespace Drupal\commerce\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\RouteMatchInterface;
+
+/**
+ * A generic controller for creating content entities.
+ */
+class AddController extends ControllerBase {
+
+  /**
+   * Displays add links for the available bundles.
+   *
+   * Redirects to the add form if there's only one bundle available.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $routeMatch
+   *   The route match.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse|array
+   *   If there's only one available bundle, a redirect response.
+   *   Otherwise, a render array with the add links for each bundle.
+   */
+  public function addPage(RouteMatchInterface $routeMatch) {
+    $routeName = $routeMatch->getRouteName();
+    $defaults = $routeMatch->getRouteObject()->getDefaults();
+    if (empty($defaults['_bundle_type'])) {
+      throw new \InvalidArgumentException(sprintf('The route "%s" must have a "_bundle_type" default parameter.', $routeName));
+    }
+
+    $formRouteName = str_replace('.add_page', '.add_form', $routeName);
+    $bundleType = $defaults['_bundle_type'];
+    $bundles = $this->entityManager()->getStorage($bundleType)->loadMultiple();
+    // Filter out the bundles the user doesn't have access to.
+    $accessControlHandler = $this->entityManager()->getAccessControlHandler($bundleType);
+    $bundles = array_filter($bundles, function($bundle) use ($accessControlHandler) {
+      return $accessControlHandler->createAccess($bundle->id());
+    });
+    // Redirect if there's only one bundle available.
+    if (count($bundles) == 1) {
+      $bundle = reset($bundles);
+
+      return $this->redirect($formRouteName, array($bundleType => $bundle->id()));
+    }
+
+    return array(
+      '#theme' => 'commerce_add_list',
+      '#bundles' => $bundles,
+      '#bundle_type' => $bundleType,
+      '#form_route_name' => $formRouteName,
+    );
+  }
+
+  /**
+   * Provides the add form for an entity of a specific bundle.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $routeMatch
+   *   The route match.
+   *
+   * @return array
+   *   The add form.
+   */
+  public function addForm(RouteMatchInterface $routeMatch) {
+    $bundle = $this->getBundleFromRouteMatch($routeMatch);
+    $entityType = $bundle->getEntityType()->getBundleOf();
+    $entity = $this->entityManager()->getStorage($entityType)->create(array(
+      'type' => $bundle->id(),
+    ));
+
+    return $this->entityFormBuilder()->getForm($entity, 'add');
+  }
+
+  /**
+   * The title callback for the add form page.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $routeMatch
+   *   The route match.
+   *
+   * @return string
+   *   The page title.
+   */
+  public function addFormTitle(RouteMatchInterface $routeMatch) {
+    $bundle = $this->getBundleFromRouteMatch($routeMatch);
+
+    return $this->t('Create @label', array('@label' => $bundle->label()));
+  }
+
+  /**
+   * Returns the bundle object from the route match.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $routeMatch
+   *   The route match.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface
+   *   The bundle object as determined from the passed-in route match.
+   */
+  protected function getBundleFromRouteMatch(RouteMatchInterface $routeMatch) {
+    // Assume that the bundle is the last route parameter.
+    $parameters = $routeMatch->getParameters()->all();
+    $bundle = end($parameters);
+
+    return $bundle;
+  }
+
+}
