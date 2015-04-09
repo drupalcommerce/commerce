ae0ca84e3ee07b31d49fdfb0278ab09cf1963a66
tavi toporjinschi <vasike@gmail.com>
2015-04-09 16:53:52 +0000

    Issues #2464371, #2464347 by bojanz, vasike: Create Cart page - New Commerce cart module.

---
+++ modules/cart/src/Controller/CartController.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\commerce_cart\Controller\CartController.
+ */
+
+namespace Drupal\commerce_cart\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Controller routines for cart routes.
+ */
+class CartController extends ControllerBase {
+
+  /**
+   * Displays the shopping cart form and associated information.
+   *
+   * @return array
+   *   A view with cart form content.
+   */
+  public function cartPage() {
+    // Get the Shopping Order id,
+    $cart_order_id = 1;
+    // To do : use the Cart settings to use a custom View for this page.
+    return views_embed_view('commerce_cart_form', 'default', array($cart_order_id));
+  }
+
+}
