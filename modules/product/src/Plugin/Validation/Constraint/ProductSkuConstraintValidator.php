32b65c45ea2d9d4b69a20ca9c54b1e482add5a25
Josh Taylor <joshuataylorx@gmail.com>
2015-04-28 00:51:06 +1000

    Issue #2478631: SKUs should only be unique on a per-store basis

--- modules/product/src/Plugin/Validation/Constraint/ProductSkuConstraintValidator.php
+++ modules/product/src/Plugin/Validation/Constraint/ProductSkuConstraintValidator.php
@@ -22,9 +22,11 @@ class ProductSkuConstraintValidator extends ConstraintValidator {
     $sku = $items->first()->value;
     if (isset($sku) && $sku !== '') {
       $productId = $items->getEntity()->product_id->value;
+      $storeId = $items->getEntity()->getStore()->id();
       $skuExists = (bool) \Drupal::entityQuery('commerce_product')
         ->condition("sku", $sku)
         ->condition('product_id', (int) $productId, '<>')
+        ->condition('store_id', $storeId)
         ->range(0, 1)
         ->count()
         ->execute();
