94138b7982043a7816971765902aac28b739bd97
Bojan Zivanovic <bojanz@gmail.com>
2015-06-21 19:21:25 +0200

    Replace t() with ->t() now that the method is no longer static.

--- modules/price/src/Plugin/Field/FieldWidget/PriceDefaultWidget.php
+++ modules/price/src/Plugin/Field/FieldWidget/PriceDefaultWidget.php
@@ -166,7 +166,7 @@ class PriceDefaultWidget extends WidgetBase implements ContainerFactoryPluginInt
     $currency = $this->currencyStorage->load($value['currency_code']);
     $value['amount'] = $this->numberFormatter->parseCurrency($value['amount'], $currency);
     if ($value['amount'] === FALSE) {
-      $formState->setError($element['amount'], t('%title is not numeric.', [
+      $formState->setError($element['amount'], $this->t('%title is not numeric.', [
         '%title' => $element['#title'],
       ]));
       return;
