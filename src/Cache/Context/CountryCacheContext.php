82a4f7e73e481156d9d22300e0c7f19de988c2fd
Jared K. Smith <jaredsmith@jaredsmith.net>
2015-09-25 15:36:40 +0200

    Create a country cache context per d.o issue 2571393

---
+++ src/Cache/Context/CountryCacheContext.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\commerce\Cache\Context\CountryCacheContext
+ */
+
+namespace Drupal\commerce\Cache\Context;
+
+use Drupal\commerce\CountryContext;
+
+/**
+ * Defines the CountryCacheContext object, for "per country" caching.
+ *
+ * Cache context ID: 'country'.
+ */
+class CountryCacheContext implements CacheContextInterface {
+
+  /**
+   * The country context.
+   * 
+   * @var \Drupal\commerce\CountryContext
+   */
+  protected $countryContext;
+  
+  /**
+   * Constructs a new CountryCacheContext object.
+   * 
+   * @param \Drupal\commerce\CountryContext $context
+   *   The country context.
+   */  
+  public function __construct(CountryContext $context) {
+    $this->countryContext = $context;
+  } 
+  
+  /**
+   * {@inheritdoc}
+   */
+  public static function getLabel() {
+    return t('Country');
+  } 
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getContext() {
+    return $this->countryContext->getCountry()->getCountryCode();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata() {
+    return new CacheableMetadata();
+  }
+
+}
