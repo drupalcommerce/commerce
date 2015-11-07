3e3e3c0aaf1c65c36e32cd2da399fb9ce7ef46af
Bojan Zivanovic <bojanz@gmail.com>
2015-11-07 18:35:20 +0100

    Fix indentation in the travis before script, make it download the profile module.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -11,13 +11,14 @@ cd "$DRUPAL_TI_DRUPAL_DIR"
 
 # Download custom branches of address and composer_manager.
 (
-	# These variables come from environments/drupal-*.sh
-	mkdir -p "$DRUPAL_TI_MODULES_PATH"
-	cd "$DRUPAL_TI_MODULES_PATH"
+    # These variables come from environments/drupal-*.sh
+    mkdir -p "$DRUPAL_TI_MODULES_PATH"
+    cd "$DRUPAL_TI_MODULES_PATH"
 
-	git clone --branch 8.x-1.x http://git.drupal.org/project/composer_manager.git
-	git clone --branch 8.x-1.x http://git.drupal.org/project/address.git
-	git clone --branch 8.x-1.x http://git.drupal.org/project/inline_entity_form.git
+    git clone --branch 8.x-1.x http://git.drupal.org/project/composer_manager.git
+    git clone --branch 8.x-1.x http://git.drupal.org/project/address.git
+    git clone --branch 8.x-1.x http://git.drupal.org/project/profile.git
+    git clone --branch 8.x-1.x http://git.drupal.org/project/inline_entity_form.git
 )
 
 # Ensure the module is linked into the codebase.
