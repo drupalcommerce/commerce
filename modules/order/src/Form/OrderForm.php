46f7970d179e94ee77aec64137ad281967b66907
mglaman <mglaman@2416470.no-reply.drupal.org>
2015-12-29 20:06:30 +0100

    Implement the two-column order edit form layout.

--- modules/order/src/Form/OrderForm.php
+++ modules/order/src/Form/OrderForm.php
@@ -7,8 +7,12 @@
 
 namespace Drupal\commerce_order\Form;
 
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Datetime\DateFormatterInterface;
 use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Form controller for the commerce_order entity edit forms.
@@ -16,70 +20,136 @@ use Drupal\Core\Form\FormStateInterface;
 class OrderForm extends ContentEntityForm {
 
   /**
+   * The date formatter.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
+   * Constructs a new OrderForm object.
+   *
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   *   The entity manager.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   The date formatter.
+   */
+  public function __construct(EntityManagerInterface $entity_manager, DateFormatterInterface $date_formatter) {
+    parent::__construct($entity_manager);
+
+    $this->dateFormatter = $date_formatter;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.manager'),
+      $container->get('date.formatter')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function form(array $form, FormStateInterface $form_state) {
     /* @var \Drupal\commerce_order\Entity\Order $order */
     $order = $this->entity;
-    $currentUser = $this->currentUser();
+    $form = parent::form($form, $form_state);
 
+    $form['#theme'] = 'commerce_order_edit_form';
+    $form['#attached']['library'][] = 'commerce_order/form';
+    // Changed must be sent to the client, for later overwrite error checking.
+    $form['changed'] = [
+      '#type' => 'hidden',
+      '#default_value' => $order->getChangedTime(),
+    ];
+
+    $last_saved = $this->dateFormatter->format($order->getChangedTime(), 'short');
+    $created = $this->dateFormatter->format($order->getCreatedTime(), 'short');
     $form['advanced'] = [
-      '#type' => 'vertical_tabs',
+      '#type' => 'container',
       '#attributes' => ['class' => ['entity-meta']],
       '#weight' => 99,
     ];
-    $form = parent::form($form, $form_state);
-
-    $form['order_state'] = [
-      '#type' => 'details',
-      '#title' => t('Order state'),
+    $form['meta'] = [
+      '#attributes' => ['class' => ['entity-meta__header']],
+      '#type' => 'container',
       '#group' => 'advanced',
-      '#attributes' => [
-        'class' => ['order-form-order-state'],
+      '#weight' => -100,
+      'state' => [
+        '#type' => 'html_tag',
+        '#tag' => 'h3',
+        '#value' => $order->getState()->getLabel(),
+        '#attributes' => [
+          'class' => 'entity-meta__title',
+        ],
+        // Hide the rendered state if there's a widget for it.
+        '#access' => empty($form['store_id']),
       ],
-      '#attached' => [
-        'library' => ['commerce_order/drupal.commerce_order'],
-      ],
-      '#weight' => 90,
-      '#optional' => TRUE,
+      'created' => $this->fieldAsReadOnly($this->t('Created'), $created),
+      'changed' => $this->fieldAsReadOnly($this->t('Last saved'), $last_saved),
     ];
-
-    if (isset($form['state'])) {
-      $form['state']['#group'] = 'order_state';
-    }
-
-    // Order authoring information for administrators.
-    $form['author'] = [
+    $form['customer'] = [
       '#type' => 'details',
-      '#title' => t('Authoring information'),
+      '#title' => t('Customer information'),
       '#group' => 'advanced',
+      '#open' => TRUE,
       '#attributes' => [
         'class' => ['order-form-author'],
       ],
-      '#attached' => [
-        'library' => ['commerce_order/drupal.commerce_order'],
-      ],
       '#weight' => 91,
-      '#optional' => TRUE,
     ];
 
+    // Show the order's store only if there are multiple available.
+    $store_query = $this->entityManager->getStorage('commerce_store')->getQuery();
+    $store_count = $store_query->count()->execute();
+    if ($store_count > 1) {
+      $store_link = $order->getStore()->toLink()->toString();
+      $form['meta']['store'] = $this->fieldAsReadOnly($this->t('Store'), $store_link);
+    }
+    // Move uid/mail widgets to the sidebar, or provide read-only alternatives.
     if (isset($form['uid'])) {
-      $form['uid']['#group'] = 'author';
+      $form['uid']['#group'] = 'customer';
     }
-
+    else {
+      $user_link = $order->getOwner()->toLink()->toString();
+      $form['customer']['uid'] = $this->fieldAsReadOnly($this->t('Customer'), $user_link);
+    }
     if (isset($form['mail'])) {
-      $form['mail']['#group'] = 'author';
+      $form['mail']['#group'] = 'customer';
     }
-
-    if (isset($form['created'])) {
-      $form['created']['#group'] = 'author';
+    else {
+      $form['customer']['mail'] = $this->fieldAsReadOnly($this->t('Contact email'), $order->getEmail());
     }
 
     return $form;
   }
 
   /**
-   * Overrides Drupal\Core\Entity\EntityFormController::save().
+   * Builds a read-only form element for a field.
+   *
+   * @param string $label
+   *   The element label.
+   * @param string $value
+   *   The element value.
+   *
+   * @return array
+   *   The form element.
+   */
+  protected function fieldAsReadOnly($label, $value) {
+    return [
+      '#type' => 'item',
+      '#wrapper_attributes' => [
+        'class' => [Html::cleanCssIdentifier(strtolower($label)), 'container-inline']
+      ],
+      '#markup' => '<h4 class="label inline">' . $label . '</h4> ' . $value,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
    */
   public function save(array $form, FormStateInterface $form_state) {
     $this->entity->save();
