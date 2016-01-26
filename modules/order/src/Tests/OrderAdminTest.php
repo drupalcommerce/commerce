493c1ef3ba384c74a415faefb16f2f16e669c2f7
tavi toporjinschi <vasike@gmail.com>
2016-01-26 12:58:39 +0000

    Issue #2656222 by vasike, bojanz: Improve tests for Commerce order entities UI/admin.

--- modules/order/src/Tests/OrderAdminTest.php
+++ modules/order/src/Tests/OrderAdminTest.php
@@ -28,6 +28,7 @@ class OrderAdminTest extends OrderTestBase {
     parent::setUp();
     \Drupal::service('module_installer')->install(['profile']);
 
+    // Create a billing profile.
     $profile_values = [
       'type' => 'billing',
       'uid' => 1,
@@ -38,33 +39,61 @@ class OrderAdminTest extends OrderTestBase {
   }
 
   /**
-   * Tests creating a Order programaticaly and through the add form.
+   * Tests creating/editing an Order.
+   * The edit form is part of creating a new order.
    */
   public function testCreateOrder() {
-    // Create a order through the add form.
+    // Create a order through the create form.
     $this->drupalGet('/admin/commerce/orders');
     $this->clickLink('Create a new order');
 
     $user = $this->loggedInUser->getAccountName() . ' (' . $this->loggedInUser->id() . ')';
-    $values = [
+    $edit = [
       'customer_type' => 'existing',
       'uid' => $user,
     ];
-    $this->drupalPostForm(NULL, $values, 'Create');
+    $this->drupalPostForm(NULL, $edit, t('Create'));
 
+    // Test the edit form page returned.
+    $this->assertResponse(200, 'The order edit form can be accessed.');
+
+    // Check the integrity of the edit form.
+    $this->assertFieldByName('billing_profile', NULL, 'Billing profile field is present');
+    $this->assertFieldByName('line_items[form][inline_entity_form][purchased_entity][0][target_id]', NULL, 'Purchased entity field is present');
+    $this->assertFieldByName('line_items[form][inline_entity_form][quantity][0][value]', NULL, 'Quantity field is present');
+    $this->assertFieldByName('line_items[form][inline_entity_form][unit_price][0][amount]', NULL, 'Unit price field is present');
+    $this->assertFieldsByValue(t('Create line item'), NULL, 'Create line item button is present');
+
+    // Submit line item values.
     $entity = $this->variation->getSku() . ' (' . $this->variation->id() . ')';
-    $values = [
+    $edit = [
       'line_items[form][inline_entity_form][purchased_entity][0][target_id]' => $entity,
       'line_items[form][inline_entity_form][quantity][0][value]' => 1,
       'line_items[form][inline_entity_form][unit_price][0][amount]' => '9.99',
     ];
-    $this->drupalPostForm(NULL, $values, 'Create line item');
+    $this->drupalPostForm(NULL, $edit, t('Create line item'));
 
-    $values = [
+    // Check the line item edit form.
+    $this->drupalPostForm(NULL, [], t('Edit'));
+    $this->assertFieldByName('line_items[form][inline_entity_form][entities][0][form][purchased_entity][0][target_id]', NULL, 'SKU field is present');
+    $this->assertFieldByName('line_items[form][inline_entity_form][entities][0][form][quantity][0][value]', NULL, 'Price field is present');
+    $this->assertFieldByName('line_items[form][inline_entity_form][entities][0][form][unit_price][0][amount]', NULL, 'Status field is present');
+    $this->assertFieldsByValue(t('Update line item'), NULL, 'Update line item button is present');
+
+    // Update/edit line item.
+    $edit = [
+      'line_items[form][inline_entity_form][entities][0][form][quantity][0][value]' => 3,
+      'line_items[form][inline_entity_form][entities][0][form][unit_price][0][amount]' => '1.11',
+    ];
+    $this->drupalPostForm(NULL, $edit, t('Update line item'));
+
+    // Submit order values.
+    $edit = [
       'billing_profile' => $this->billingProfile->id(),
     ];
-    $this->drupalPostForm(NULL, $values, t('Save'));
+    $this->drupalPostForm(NULL, $edit, t('Save'));
 
+    // Test results.
     $order_number = $this->cssSelect('tr td.views-field-order-number');
     $this->assertEqual(count($order_number), 1, 'Order exists in the table.');
   }
@@ -81,15 +110,19 @@ class OrderAdminTest extends OrderTestBase {
     $order_exists = (bool) Order::load($order->id());
     $this->assertTrue($order_exists, 'The order has been created in the database.');
 
-    $this->drupalGet('admin/commerce/orders/' . $order->id() . '/delete');
+    // Delete the order created.
+    $this->drupalGet($order->toUrl('delete-form'));
+    $this->assertResponse(200, 'The order delete form can be accessed.');
     $this->assertRaw(t('Are you sure you want to delete the order %label?', [
       '%label' => $order->label(),
     ]));
     $this->assertText(t('This action cannot be undone.'), 'The order deletion confirmation form is available');
     $this->drupalPostForm(NULL, NULL, t('Delete'));
+
     // Remove the entity from cache and check if the order is deleted.
     \Drupal::service('entity_type.manager')->getStorage('commerce_order')->resetCache([$order->id()]);
     $order_exists = (bool) Order::load('commerce_order', $order->id());
     $this->assertFalse($order_exists, 'The order has been deleted from the database.');
   }
+
 }
