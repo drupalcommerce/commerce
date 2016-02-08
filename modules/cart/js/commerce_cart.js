10d2ccd1734513be896b0cbfa8ba5a3e6ea8691b
Harings Rob <haringsrob@gmail.com>
2016-02-08 21:06:17 +0100

    2659080: Make cart scrollable if greater then window + Better positioning

--- modules/cart/js/commerce_cart.js
+++ modules/cart/js/commerce_cart.js
@@ -8,12 +8,32 @@
 
   Drupal.behaviors.commerceCartBlock = {
     attach: function (context) {
-      var $context = $(context);
-      var $cart = $context.find('.cart--cart-block');
-      var $cartContents = $cart.find('.cart-block--contents');
+      var $context = $(context),
+          $cart = $context.find('.cart--cart-block'),
+          $cartButton = $context.find('.cart-block--link__expand'),
+          $cartContents = $cart.find('.cart-block--contents'),
+          $windowHeight = $(window).height(),
+          $windowWidth = $(window).width();
 
       if ($cartContents.length > 0) {
-        $cart.click(function (e) {
+        // Get the shopping cart height + the offset to the top.
+        var $cartOffsetTop = $cartContents.offset().top,
+            $cartHeight = $cartContents.height() + $cartOffsetTop;
+        // If the cart size is bigger then the viewport, we should make it scrollable. And set a max-height.
+        if ($cartHeight >= $windowHeight) {
+          $cartContents.addClass('is-scrollable');
+        }
+
+        // Get the shopping cart width + the offset to the left.
+        var $cartOffsetLeft = $cart.offset().left,
+            $cartWidth = $cartContents.width() + $cartOffsetLeft;
+        // If the cart goes out of the viewport we should align it right.
+        if ($cartWidth > $windowWidth) {
+          $cartContents.addClass('is-outside');
+        }
+
+        // Expand the block when the link is clicked.
+        $cartButton.click(function (e) {
           e.preventDefault();
           $cartContents
             .toggleClass('cart-block--contents__expanded')
