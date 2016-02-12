62cf8d8201784f7b634cc0b705ff907022978646
tortelduif <tortelduif@1742028.no-reply.drupal.org>
2016-02-12 08:36:48 -0600

    Issue #2659080: Add overflow:scroll to the cart dropdown

--- modules/cart/js/commerce_cart.js
+++ modules/cart/js/commerce_cart.js
@@ -10,17 +10,27 @@
     attach: function (context) {
       var $context = $(context);
       var $cart = $context.find('.cart--cart-block');
+      var $cartButton = $context.find('.cart-block--link__expand');
       var $cartContents = $cart.find('.cart-block--contents');
 
       if ($cartContents.length > 0) {
-        $cart.click(function (e) {
+        // Expand the block when the link is clicked.
+        $cartButton.on('click', function (e) {
+          // Prevent it from going to the cart.
           e.preventDefault();
+          // Get the shopping cart width + the offset to the left.
+          var windowWidth = $(window).width();
+          var cartWidth = $cartContents.width() + $cart.offset().left;
+          // If the cart goes out of the viewport we should align it right.
+          if (cartWidth > windowWidth) {
+            $cartContents.addClass('is-outside-horizontal');
+          }
+          // Toggle the expanded class.
           $cartContents
             .toggleClass('cart-block--contents__expanded')
             .slideToggle();
         });
       }
-
     }
   };
 })(jQuery, Drupal, drupalSettings);
