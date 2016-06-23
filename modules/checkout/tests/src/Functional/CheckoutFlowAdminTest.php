77915ddce920598b2758a9b406be1a17777d8f67
ndf <nielsdefeyter@gmail.com>
2016-06-23 13:04:13 +0200

    improved test coverage

--- modules/checkout/tests/src/Functional/CheckoutFlowAdminTest.php
+++ modules/checkout/tests/src/Functional/CheckoutFlowAdminTest.php
@@ -37,10 +37,109 @@ class CheckoutFlowAdminTest extends CommerceBrowserTestBase {
   /**
    * Tests the checkout-workflow admin pages.
    */
-  public function testCheckoutFlowAdminPages() {
+  public function testCheckoutFlowAdminPaneWeights() {
     // Goto the administration page for Checkout Workflows.
     $this->drupalGet('/admin/commerce/config/checkout-flows/manage/default');
     $this->assertSession()->statusCodeEquals(200);
+
+    $this->assertSession()->pageTextContains('No pane is displayed');
+  }
+
+  /**
+   * Test Checkout Label.
+   */
+  public function testCheckoutFlowAdminLabel() {
+    $admin_url = '/admin/commerce/config/checkout-flows/manage/default';
+    $form_field = 'label';
+    $default_value = 'Default';
+    $new_value = 'Changed label';
+
+    // Goto the administration page for Checkout Workflows.
+    $this->drupalGet($admin_url);
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Validate default.
+    $this->assertSession()->fieldValueEquals($form_field, $default_value);
+
+    // Change the Label value.
+    $values = [
+      $form_field => $new_value,
+    ];
+    $this->submitForm($values, t('Save'));
+    $this->assertSession()->pageTextContains($new_value);
+  }
+
+  /**
+   * Tests Order summary view.
+   */
+  public function testCheckoutFlowAdminOrderSummaryView() {
+    $admin_url = '/admin/commerce/config/checkout-flows/manage/default';
+    $form_field = 'configuration[order_summary_view]';
+    $default_value = 'commerce_checkout_order_summary';
+    $new_value = '';
+
+    // Goto the administration page for Checkout Workflows.
+    $this->drupalGet($admin_url);
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Validate that default value is 'commerce_checkout_order_summary'.
+    $this->assertSession()->pageTextContains('Order summary view');
+    $this->assertSession()->fieldExists($form_field);
+    $this->assertSession()->fieldValueEquals($form_field, $default_value);
+    $this->assertSession()->fieldValueNotEquals($form_field, $new_value);
+
+    // Set to "-- None --".
+    $values = [
+      $form_field => $new_value,
+    ];
+    $this->submitForm($values, t('Save'));
+    $this->drupalGet($admin_url);
+    $this->assertSession()->fieldValueEquals($form_field, $new_value);
+
+    // Set back to "Checkout Order Summary".
+    $values = [
+      $form_field => $default_value,
+    ];
+    $this->submitForm($values, t('Save'));
+    $this->drupalGet($admin_url);
+    $this->assertSession()->fieldValueEquals($form_field, $default_value);
+  }
+
+  /**
+   * Tests Display checkout pages.
+   */
+  public function testCheckoutFlowAdminDisplayCheckoutProgress() {
+    $admin_url = '/admin/commerce/config/checkout-flows/manage/default';
+    $form_field = 'configuration[display_checkout_progress]';
+    $default_value = '1';
+    $new_value = '0';
+
+    // Goto the administration page for Checkout Workflows.
+    $this->drupalGet($admin_url);
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Validate that default value is Enabled.
+    $this->assertSession()->pageTextContains('Used by the checkout progress block to determine visibility');
+    $this->assertSession()->fieldExists($form_field);
+    $this->assertSession()->checkboxChecked($form_field);
+
+    // Change to new-value.
+    $values = [
+      $form_field => $new_value,
+    ];
+    $this->submitForm($values, t('Save'));
+    $this->drupalGet($admin_url);
+    $this->assertSession()->checkboxNotChecked($form_field);
+    $this->assertSession()->fieldValueEquals($form_field, $new_value);
+
+    // Set back to default.
+    $values = [
+      $form_field => $default_value,
+    ];
+    $this->submitForm($values, t('Save'));
+    $this->drupalGet($admin_url);
+    $this->assertSession()->checkboxChecked($form_field);
+    $this->assertSession()->fieldValueEquals($form_field, $default_value);
   }
 
 }
