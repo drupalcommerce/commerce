d302eff5734aae6b63442184d7980a0ef09a4708
Matt Glaman <nmd.matt@gmail.com>
2016-07-11 02:14:57 -0500

    Issue #2763695: Rename to commerce_promotion

--- modules/promotion/src/PromotionStorageInterface.php
+++ modules/promotion/src/PromotionStorageInterface.php
@@ -1,25 +1,25 @@
 <?php
 
-namespace Drupal\commerce_discount;
+namespace Drupal\commerce_promotion;
 
 use Drupal\commerce_order\Entity\OrderTypeInterface;
 use Drupal\commerce_store\Entity\StoreInterface;
 
 /**
- * Defines the interface for discount storage.
+ * Defines the interface for promotion storage.
  */
-interface DiscountStorageInterface {
+interface PromotionStorageInterface {
 
   /**
-   * Loads the valid discounts for the given order type and store.
+   * Loads the valid promotions for the given order type and store.
    *
    * @param \Drupal\commerce_order\Entity\OrderTypeInterface $order_type
    *   The order type.
    * @param \Drupal\commerce_store\Entity\StoreInterface $store
    *   The store.
    *
-   * @return \Drupal\commerce_discount\Entity\DiscountInterface[]
-   *   The valid discounts.
+   * @return \Drupal\commerce_promotion\Entity\PromotionInterface[]
+   *   The valid promotions.
    */
   public function loadValid(OrderTypeInterface $order_type, StoreInterface $store);
 
