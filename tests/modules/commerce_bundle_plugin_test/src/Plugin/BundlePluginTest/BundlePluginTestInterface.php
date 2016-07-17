9bd1acf7aef38d4e0b3e90c3284635fd57cc5618
Bojan Zivanovic <bojanz@gmail.com>
2016-07-17 17:21:30 +0200

    Complete the bundle plugin tests.

---
+++ tests/modules/commerce_bundle_plugin_test/src/Plugin/BundlePluginTest/BundlePluginTestInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\commerce_bundle_plugin_test\Plugin\BundlePluginTest;
+
+use Drupal\commerce\BundlePluginInterface;
+
+interface BundlePluginTestInterface extends BundlePluginInterface {
+
+}
