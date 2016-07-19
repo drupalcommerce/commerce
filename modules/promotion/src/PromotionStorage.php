798c3e755e24dfd68da121375fbdaec59e6828e0
mglaman <mglaman@2416470.no-reply.drupal.org>
2016-07-19 15:41:49 -0500

    Issue #2769425 by mglaman: Create coupon entity

--- modules/promotion/src/PromotionStorage.php
+++ modules/promotion/src/PromotionStorage.php
@@ -4,6 +4,7 @@ namespace Drupal\commerce_promotion;
 
 use Drupal\commerce\CommerceContentEntityStorage;
 use Drupal\commerce_order\Entity\OrderTypeInterface;
+use Drupal\commerce_promotion\Entity\CouponInterface;
 use Drupal\commerce_store\Entity\StoreInterface;
 
 /**
@@ -12,9 +13,17 @@ use Drupal\commerce_store\Entity\StoreInterface;
 class PromotionStorage extends CommerceContentEntityStorage implements PromotionStorageInterface {
 
   /**
-   * {@inheritdoc}
+   * Helper method to return base query for valid promotions.
+   *
+   * @param \Drupal\commerce_order\Entity\OrderTypeInterface $order_type
+   *   The order type.
+   * @param \Drupal\commerce_store\Entity\StoreInterface $store
+   *   The store.
+   *
+   * @return \Drupal\Core\Entity\Query\QueryInterface
+   *   The entity query.
    */
-  public function loadValid(OrderTypeInterface $order_type, StoreInterface $store) {
+  protected function loadValidQuery(OrderTypeInterface $order_type, StoreInterface $store) {
     $query = $this->getQuery();
 
     $or_condition = $query->orConditionGroup()
@@ -26,6 +35,14 @@ class PromotionStorage extends CommerceContentEntityStorage implements Promotion
       ->condition('start_date', gmdate('Y-m-d'), '<=')
       ->condition('status', TRUE)
       ->condition($or_condition);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadValid(OrderTypeInterface $order_type, StoreInterface $store) {
+    $query = $this->loadValidQuery($order_type, $store);
     $result = $query->execute();
     if (empty($result)) {
       return [];
@@ -33,6 +50,22 @@ class PromotionStorage extends CommerceContentEntityStorage implements Promotion
     $promotions = $this->loadMultiple($result);
 
     return $promotions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadByCoupon(OrderTypeInterface $order_type, StoreInterface $store, CouponInterface $coupon) {
+    $query = $this->loadValidQuery($order_type, $store);
+    $query->condition('coupons', $coupon->id());
+    $result = $query->execute();
+    if (empty($result)) {
+      return [];
+    }
+    $promotions = $this->loadMultiple($result);
+
+    return reset($promotions);
+
   }
 
 }
