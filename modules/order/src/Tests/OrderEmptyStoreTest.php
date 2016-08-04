d5d603351dee6625902fd5b3b4621567109219e5
Joris Vercammen <botchcake@gmail.com>
2016-08-04 13:56:07 +0200

    Issue #2664676: Add test cases for empty store

---
+++ modules/order/src/Tests/OrderEmptyStoreTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\commerce_order\Tests;
+
+use Drupal\commerce\Tests\CommerceTestBase;
+use Drupal\Core\Url;
+
+/**
+ * Empty Store order page test.
+ *
+ * @group commerce
+ */
+class OrderEmptyStoreTest extends CommerceTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'commerce_order',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getAdministratorPermissions() {
+    return array_merge([
+      'administer orders',
+      'administer order types',
+    ], parent::getAdministratorPermissions());
+  }
+
+  /**
+   * Tests order create page.
+   */
+  function testCreateOrderPage() {
+    $this->drupalGet('admin/commerce/orders');
+    $this->clickLink('Create a new order');
+
+    // Check the link is present.
+    $this->assertLink(t('Add a new store.'));
+    $this->assertLinkByHref(Url::fromRoute('entity.commerce_store.add_page')->toString());
+  }
+}
