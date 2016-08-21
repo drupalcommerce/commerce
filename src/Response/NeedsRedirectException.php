fe24b3eb0f9daa355e35bfdd9602b9d497f1f696
Bojan Zivanovic <bojanz@gmail.com>
2016-08-22 00:13:27 +0200

    Issue #2753831 by mglaman: Add the ability to throw a redirect exception

---
+++ src/Response/NeedsRedirectException.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\commerce\Response;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Form\EnforcedResponseException;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+
+/**
+ * Provides an exception that represents the need for an HTTP redirect.
+ *
+ * Allows nested forms to perform HTTP redirects in an easy way.
+ */
+class NeedsRedirectException extends EnforcedResponseException {
+
+  /**
+   * Constructs a new NeedsRedirectException object.
+   *
+   * @param string $url
+   *    The URL to redirect to.
+   * @param int $status_code
+   *    The redirect status code.
+   * @param string[] $headers
+   *    Headers to pass with the redirect.
+   */
+  public function __construct($url, $status_code = 302, array $headers = []) {
+    if (!UrlHelper::isValid($url)) {
+      throw new \InvalidArgumentException('Invalid URL provided.');
+    }
+
+    parent::__construct(new TrustedRedirectResponse($url, $status_code, $headers));
+  }
+
+}
