350d1846a04980e57d140a188c08bdeacbc21187
Steve Oliver <steve@circatree.com>
2016-09-23 13:35:53 -0700

    Issue #2804227 Add total_paid field and Order::getBalance() and related methods.

--- modules/payment/src/Entity/Payment.php
+++ modules/payment/src/Entity/Payment.php
@@ -175,6 +175,7 @@ class Payment extends ContentEntityBase implements PaymentInterface {
    */
   public function setAmount(Price $amount) {
     $this->set('amount', $amount);
+    $this->getOrder()->addPayment($amount);
     return $this;
   }
 
@@ -192,6 +193,7 @@ class Payment extends ContentEntityBase implements PaymentInterface {
    */
   public function setRefundedAmount(Price $refunded_amount) {
     $this->set('refunded_amount', $refunded_amount);
+    $this->getOrder()->subtractPayment($refunded_amount);
     return $this;
   }
 
