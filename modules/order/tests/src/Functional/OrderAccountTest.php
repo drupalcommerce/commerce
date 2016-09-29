381d1d23c81c19ddc5aaf73639187a21e303e09a
Steve Oliver <steve@circatree.com>
2016-09-29 11:18:03 -0700

    Move 'guest checkout order -> new account order' test from Checkout to Order module (first failing test).

---
+++ modules/order/tests/src/Functional/OrderAccountTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\Tests\commerce_order\Functional;
+
+use Drupal\commerce_order\Entity\Order;
+
+/**
+ * Tests the order account (owner) functionality.
+ *
+ * @group commerce
+ */
+class OrderAccountTest extends OrderBrowserTestBase {
+
+  /**
+   * Tests assigning guest orders to newly created users (after checkout).
+   */
+  public function testNewAccountOwnsOrderAfterGuestCheckout() {
+    $order_item = $this->createEntity('commerce_order_item', [
+      'type' => 'product_variation',
+    ]);
+    $order = $this->createEntity('commerce_order', [
+      'type' => 'default',
+      'mail' => 'guest@example.com',
+      'order_items' => [$order_item],
+    ]);
+    $order = Order::load($order->id());
+
+    $this->assertEmpty($order->getOwnerId(), 'The guest order has no owner account.');
+    $user = $this->createUser([], 'guest');
+    $this->assertEquals($order->getOwnerId(), $user->id(), 'New user account owns previous guest order.');
+  }
+
+}
