e3b7061ea600d5ee698d727ac01a13510a5cd748
steveoliver <steveoliver@42327.no-reply.drupal.org>
2016-10-06 15:44:03 +0200

    Issue #2799589 by steveoliver, bojanz: Add product variation title widget

--- modules/product/src/Plugin/Field/FieldWidget/ProductVariationAttributesWidget.php
+++ modules/product/src/Plugin/Field/FieldWidget/ProductVariationAttributesWidget.php
@@ -2,27 +2,19 @@
 
 namespace Drupal\commerce_product\Plugin\Field\FieldWidget;
 
-use Drupal\commerce_product\Entity\ProductVariation;
 use Drupal\commerce_product\Entity\ProductVariationInterface;
-use Drupal\commerce_product\Event\ProductEvents;
-use Drupal\commerce_product\Event\ProductVariationAjaxChangeEvent;
 use Drupal\commerce_product\ProductAttributeFieldManagerInterface;
 use Drupal\Component\Utility\Html;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'commerce_product_variation_attributes' widget.
- *
- * The widget form depends on the 'product' being present in $form_state.
- *
- * @see \Drupal\commerce_product\Plugin\Field\FieldFormatter\AddToCartFormatter::viewElements().
  *
  * @FieldWidget(
  *   id = "commerce_product_variation_attributes",
@@ -32,7 +24,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   }
  * )
  */
-class ProductVariationAttributesWidget extends WidgetBase implements ContainerFactoryPluginInterface {
+class ProductVariationAttributesWidget extends ProductVariationWidgetBase implements ContainerFactoryPluginInterface {
 
   /**
    * The attribute field manager.
@@ -40,13 +32,6 @@ class ProductVariationAttributesWidget extends WidgetBase implements ContainerFa
    * @var \Drupal\commerce_product\ProductAttributeFieldManagerInterface
    */
   protected $attributeFieldManager;
-
-  /**
-   * The product variation storage.
-   *
-   * @var \Drupal\commerce_product\ProductVariationStorageInterface
-   */
-  protected $variationStorage;
 
   /**
    * The product attribute storage.
@@ -68,16 +53,15 @@ class ProductVariationAttributesWidget extends WidgetBase implements ContainerFa
    *   The widget settings.
    * @param array $third_party_settings
    *   Any third party settings.
-   * @param \Drupal\commerce_product\ProductAttributeFieldManagerInterface $attribute_field_manager
-   *   The attribute field manager.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
+   * @param \Drupal\commerce_product\ProductAttributeFieldManagerInterface $attribute_field_manager
+   *   The attribute field manager.
    */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, ProductAttributeFieldManagerInterface $attribute_field_manager, EntityTypeManagerInterface $entity_type_manager) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager, ProductAttributeFieldManagerInterface $attribute_field_manager) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings, $entity_type_manager);
 
     $this->attributeFieldManager = $attribute_field_manager;
-    $this->variationStorage = $entity_type_manager->getStorage('commerce_product_variation');
     $this->attributeStorage = $entity_type_manager->getStorage('commerce_product_attribute');
   }
 
@@ -91,18 +75,9 @@ class ProductVariationAttributesWidget extends WidgetBase implements ContainerFa
       $configuration['field_definition'],
       $configuration['settings'],
       $configuration['third_party_settings'],
-      $container->get('commerce_product.attribute_field_manager'),
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('commerce_product.attribute_field_manager')
     );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function isApplicable(FieldDefinitionInterface $field_definition) {
-    $entity_type = $field_definition->getTargetEntityTypeId();
-    $field_name = $field_definition->getName();
-    return $entity_type == 'commerce_order_item' && $field_name == 'purchased_entity';
   }
 
   /**
@@ -207,21 +182,7 @@ class ProductVariationAttributesWidget extends WidgetBase implements ContainerFa
   }
 
   /**
-   * {@inheritdoc}
-   */
-  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
-    // Map the variation form value to the expected field structure.
-    foreach ($values as $key => $value) {
-      $values[$key] = [
-        'target_id' => $value['variation'],
-      ];
-    }
-
-    return $values;
-  }
-
-  /**
-   * Selects a product variation based on user input having attribute values.
+   * Selects a product variation from user input.
    *
    * If there's no user input (form viewed for the first time), the default
    * variation is returned.
@@ -335,30 +296,6 @@ class ProductVariationAttributesWidget extends WidgetBase implements ContainerFa
     }
 
     return $values;
-  }
-
-  /**
-   * Ajax callback.
-   */
-  public static function ajaxRefresh(array $form, FormStateInterface $form_state) {
-    /** @var \Drupal\Core\Render\MainContent\MainContentRendererInterface $ajax_renderer */
-    $ajax_renderer = \Drupal::service('main_content_renderer.ajax');
-    $request = \Drupal::request();
-    $route_match = \Drupal::service('current_route_match');
-    /** @var \Drupal\Core\Ajax\AjaxResponse $response */
-    $response = $ajax_renderer->renderResponse($form, $request, $route_match);
-
-    $variation = ProductVariation::load($form_state->get('selected_variation'));
-    /** @var \Drupal\commerce_product\ProductVariationFieldRendererInterface $variation_field_renderer */
-    $variation_field_renderer = \Drupal::service('commerce_product.variation_field_renderer');
-    $view_mode = $form_state->get('form_display')->getMode();
-    $variation_field_renderer->replaceRenderedFields($response, $variation, $view_mode);
-    // Allow modules to add arbitrary ajax commands to the response.
-    $event = new ProductVariationAjaxChangeEvent($variation, $response, $view_mode);
-    $event_dispatcher = \Drupal::service('event_dispatcher');
-    $event_dispatcher->dispatch(ProductEvents::PRODUCT_VARIATION_AJAX_CHANGE, $event);
-
-    return $response;
   }
 
 }
