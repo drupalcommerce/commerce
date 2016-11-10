d369758e5d2117b88553cc4eecb4f72902b06945
Steve Oliver <steve@circatree.com>
2016-11-10 11:33:28 -0800

    Add context in add to cart form.

--- modules/cart/src/Form/AddToCartForm.php
+++ modules/cart/src/Form/AddToCartForm.php
@@ -177,9 +177,10 @@ class AddToCartForm extends ContentEntityForm {
   public function buildEntity(array $form, FormStateInterface $form_state) {
     /** @var \Drupal\commerce_order\Entity\OrderItemInterface $entity */
     $entity = parent::buildEntity($form, $form_state);
+    $context = new Context($entity, \Drupal::currentUser(), $this->selectStore($entity));
     // Now that the purchased entity is set, populate the title and price.
     $entity->setTitle($entity->getPurchasedEntity()->getOrderItemTitle());
-    $entity->setUnitPrice($this->chainPriceResolver->resolve($entity->getPurchasedEntity()));
+    $entity->setUnitPrice($this->chainPriceResolver->resolve($entity->getPurchasedEntity()), $context);
 
     return $entity;
   }
