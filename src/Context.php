4392d79f9df5f46f1e27236321853b6d680b2a35
Steve Oliver <steve@circatree.com>
2016-11-10 11:15:34 -0800

    Coding standards.

--- src/Context.php
+++ src/Context.php
@@ -32,7 +32,7 @@ final class Context {
   /**
    * Constructs a new Commerce Context object.
    *
-   * @param \Drupal\user\UserInterface
+   * @param \Drupal\user\UserInterface $user
    *   The user entity.
    * @param \Drupal\commerce_store\Entity\StoreInterface $store
    *   The store entity.
@@ -49,6 +49,7 @@ final class Context {
    * Gets the user entity.
    *
    * @return \Drupal\user\UserInterface
+   *   The user entity.
    */
   public function getUser() {
     return $this->user;
@@ -58,6 +59,7 @@ final class Context {
    * Gets the store entity.
    *
    * @return \Drupal\commerce_store\Entity\Store
+   *   The store entity.
    */
   public function getStore() {
     return $this->store;
@@ -67,9 +69,10 @@ final class Context {
    * Gets the date.
    *
    * @return \Drupal\Core\Datetime\DrupalDateTime
+   *   The date object.
    */
   public function getDate() {
     return $this->date;
   }
-}
 
+}
