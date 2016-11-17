3f3bc1dcc48c35799d73cde7a7e8ea4bf4f2b46e
agoradesign <agoradesign@1183886.no-reply.drupal.org>
2016-11-17 22:00:23 +0100

    adding explicit NULL return statements to chain resolvers

--- modules/order/src/Resolver/ChainOrderTypeResolver.php
+++ modules/order/src/Resolver/ChainOrderTypeResolver.php
@@ -50,6 +50,7 @@ class ChainOrderTypeResolver implements ChainOrderTypeResolverInterface {
         return $result;
       }
     }
+    return NULL;
   }
 
 }
--- modules/price/src/Resolver/ChainPriceResolver.php
+++ modules/price/src/Resolver/ChainPriceResolver.php
@@ -50,6 +50,7 @@ class ChainPriceResolver implements ChainPriceResolverInterface {
         return $result;
       }
     }
+    return NULL;
   }
 
 }
