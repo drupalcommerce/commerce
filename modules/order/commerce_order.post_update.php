ed625b3d4be2936a265e77e7e8fe39492e458713
Bojan Zivanovic <bojanz@gmail.com>
2016-11-29 23:57:54 +0100

    Issue #2757283 by mglaman, cornifex: Re-template the admin and user order views

--- modules/order/commerce_order.post_update.php
+++ modules/order/commerce_order.post_update.php
@@ -53,3 +53,36 @@ function commerce_order_post_update_2() {
     }
   }
 }
+
+/**
+ * Revert the Order and Profile entity view displays.
+ */
+function commerce_order_post_update_3() {
+  /** @var \Drupal\commerce\Config\ConfigUpdaterInterface $config_updater */
+  $config_updater = \Drupal::service('commerce.config_updater');
+
+  $views = [
+    'core.entity_view_display.commerce_order.default.default',
+    'core.entity_view_display.commerce_order.default.user',
+    'core.entity_view_display.profile.customer.default',
+  ];
+  $result = $config_updater->revert($views, FALSE);
+
+  $success_results = $result->getSucceeded();
+  $failure_results = $result->getFailed();
+  if ($success_results) {
+    $message = t('Succeeded:') . '<br>';
+    foreach ($success_results as $success_message) {
+      $message .= $success_message . '<br>';
+    }
+    $message .= '<br>';
+  }
+  if ($failure_results) {
+    $message .= t('Failed:') . '<br>';
+    foreach ($failure_results as $failure_message) {
+      $message .= $failure_message . '<br>';
+    }
+  }
+
+  return $message;
+}
