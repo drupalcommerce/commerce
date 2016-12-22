0125ea62fa0bd0c3930fdd044b73e2b4c49363fc
Matt Glaman <nmd.matt@gmail.com>
2016-12-22 08:08:23 -0600

    Issue #2827747: Follow up tweaks

--- modules/cart/src/EventSubscriber/CartEventSubscriber.php
+++ modules/cart/src/EventSubscriber/CartEventSubscriber.php
@@ -3,15 +3,33 @@
 namespace Drupal\commerce_cart\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\commerce_cart\CartProviderInterface;
 use Drupal\state_machine\Event\WorkflowTransitionEvent;
 
 /**
- * Event reactions for commerce_cart.
+ * Reacts to order events.
  */
 class CartEventSubscriber implements EventSubscriberInterface {
 
   /**
-   * @inheritdoc
+   * The cart provider.
+   *
+   * @var \Drupal\commerce_cart\CartProviderInterface
+   */
+  protected $cartProvider;
+
+  /**
+   * Constructs a new CartEventSubscriber object.
+   *
+   * @param \Drupal\commerce_cart\CartProviderInterface $cart_provider
+   *   The cart provider.
+   */
+  public function __construct(CartProviderInterface $cart_provider) {
+    $this->cartProvider = $cart_provider;
+  }
+
+  /**
+   * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
     $events = ['commerce_order.place.pre_transition' => 'finalizeCart'];
@@ -24,10 +42,13 @@ class CartEventSubscriber implements EventSubscriberInterface {
    * Reacts to order "place" transition event.
    *
    * @param \Drupal\state_machine\Event\WorkflowTransitionEvent $event
+   *   The workflow transition event.
    */
   public function finalizeCart(WorkflowTransitionEvent $event) {
     $order = $event->getEntity();
-    \Drupal::service('commerce_cart.cart_provider')->finalizeCart($order, FALSE);
+    if ($order->cart->value == TRUE) {
+      $this->cartProvider->finalizeCart($order, FALSE);
+    }
   }
 
 }
