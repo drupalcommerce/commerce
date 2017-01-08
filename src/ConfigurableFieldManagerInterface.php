74c655e8e4b0a45aff651befebef2fe42eefc114
Bojan Zivanovic <bojanz@gmail.com>
2017-01-08 18:52:59 +0100

    Issue #2841910: Create a ConfigurableFieldManager

---
+++ src/ConfigurableFieldManagerInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\commerce;
+
+/**
+ * Manages configurable fields based on field definitions.
+ *
+ * Allows for an easier way to create/delete configurable fields from code.
+ */
+interface ConfigurableFieldManagerInterface {
+
+  /**
+   * Creates a configurable field from the given field definition.
+   *
+   * @param \Drupal\commerce\BundleFieldDefinition $field_definition
+   *   The field definition.
+   * @param bool $lock
+   *   Whether the created field should be locked.
+   *
+   * @throws \InvalidArgumentException
+   *   Thrown when given an incomplete field definition (missing name,
+   *   target entity type ID, or target bundle).
+   * @throws \RuntimeException
+   *   Thrown when a field with the same name already exists.
+   */
+  public function createField(BundleFieldDefinition $field_definition, $lock = TRUE);
+
+  /**
+   * Deletes the configurable field created from the given field definition.
+   *
+   * @param \Drupal\commerce\BundleFieldDefinition $field_definition
+   *   The field definition.
+   *
+   * @throws \InvalidArgumentException
+   *   Thrown when given an incomplete field definition (missing name,
+   *   target entity type ID, or target bundle).
+   * @throws \RuntimeException
+   *   Thrown when no matching field was found.
+   */
+  public function deleteField(BundleFieldDefinition $field_definition);
+
+}
