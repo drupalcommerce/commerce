a465cf4a827a5ae031e55836e276a8c05561e388
bmcclure <bmcclure@278485.no-reply.drupal.org>
2017-02-14 10:44:25 -0600

    Issue #2852363 by bmcclure: Provide a Views default argument for the current user's cart ID

---
+++ modules/cart/src/Plugin/views/argument_default/CurrentCart.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Drupal\commerce_cart\Plugin\views\argument_default;
+
+use Drupal\commerce_cart\CartProviderInterface;
+use Drupal\Core\Cache\CacheableDependencyInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Default argument plugin for the current user's cart.
+ *
+ * @ViewsArgumentDefault(
+ *   id = "current_cart",
+ *   title = @Translation("Cart ID from current user")
+ * )
+ */
+class CurrentCart extends ArgumentDefaultPluginBase implements CacheableDependencyInterface {
+
+  /**
+   * The cart provider.
+   *
+   * @var \Drupal\commerce_cart\CartProviderInterface
+   */
+  protected $cartProvider;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a PluginBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param CartProviderInterface $cart_provider
+   *   The cart provider.
+   * @param EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CartProviderInterface $cart_provider, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->cartProvider = $cart_provider;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('commerce_cart.cart_provider'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getArgument() {
+    $carts = $this->getCarts();
+
+    $ids = [];
+
+    foreach ($carts as $cart) {
+      $ids[] = $cart->id();
+    }
+
+    return implode('+', $ids);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return ['user', 'session'];
+  }
+
+  /**
+   * @return \Drupal\commerce_order\Entity\OrderInterface[]
+   */
+  protected function getCarts() {
+    /** @var \Drupal\commerce_order\Entity\OrderInterface[] $carts */
+    $carts = $this->cartProvider->getCarts();
+    $carts = array_filter($carts, function ($cart) {
+      /** @var \Drupal\commerce_order\Entity\OrderInterface $cart */
+      // There is a chance the cart may have converted from a draft order, but
+      // is still in session. Such as just completing check out. So we verify
+      // that the cart is still a cart.
+      return $cart->hasItems() && $cart->cart->value;
+    });
+
+    return $carts;
+  }
+
+}
+
