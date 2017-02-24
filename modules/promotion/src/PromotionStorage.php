42796fc2ec96abb0a97819c99e74015ed29a85b5
Matt Glaman <nmd.matt@gmail.com>
2017-02-23 22:34:12 -0600

    Issue #2855119 by mglaman, niko-: Use a separate order processor for coupon based promotions

--- modules/promotion/src/PromotionStorage.php
+++ modules/promotion/src/PromotionStorage.php
@@ -13,36 +13,13 @@ use Drupal\commerce_store\Entity\StoreInterface;
 class PromotionStorage extends CommerceContentEntityStorage implements PromotionStorageInterface {
 
   /**
-   * Helper method to return base query for valid promotions.
-   *
-   * @param \Drupal\commerce_order\Entity\OrderTypeInterface $order_type
-   *   The order type.
-   * @param \Drupal\commerce_store\Entity\StoreInterface $store
-   *   The store.
-   *
-   * @return \Drupal\Core\Entity\Query\QueryInterface
-   *   The entity query.
-   */
-  protected function loadValidQuery(OrderTypeInterface $order_type, StoreInterface $store) {
-    $query = $this->getQuery();
-
-    $or_condition = $query->orConditionGroup()
-      ->condition('end_date', gmdate('Y-m-d'), '>=')
-      ->notExists('end_date', gmdate('Y-m-d'));
-    $query
-      ->condition('stores', [$store->id()], 'IN')
-      ->condition('order_types', [$order_type->id()], 'IN')
-      ->condition('start_date', gmdate('Y-m-d'), '<=')
-      ->condition('status', TRUE)
-      ->condition($or_condition);
-    return $query;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function loadValid(OrderTypeInterface $order_type, StoreInterface $store) {
-    $query = $this->loadValidQuery($order_type, $store);
+    $query = $this->buildLoadQuery($order_type, $store);
+    // Only load promotions without coupons. Promotions with coupons are loaded
+    // coupon-first in a different process.
+    $query->notExists('coupons');
     $result = $query->execute();
     if (empty($result)) {
       return [];
@@ -56,7 +33,7 @@ class PromotionStorage extends CommerceContentEntityStorage implements Promotion
    * {@inheritdoc}
    */
   public function loadByCoupon(OrderTypeInterface $order_type, StoreInterface $store, CouponInterface $coupon) {
-    $query = $this->loadValidQuery($order_type, $store);
+    $query = $this->buildLoadQuery($order_type, $store);
     $query->condition('coupons', $coupon->id());
     $result = $query->execute();
     if (empty($result)) {
@@ -66,6 +43,32 @@ class PromotionStorage extends CommerceContentEntityStorage implements Promotion
 
     return reset($promotions);
 
+  }
+
+  /**
+   * Builds the base query for loading valid promotions.
+   *
+   * @param \Drupal\commerce_order\Entity\OrderTypeInterface $order_type
+   *   The order type.
+   * @param \Drupal\commerce_store\Entity\StoreInterface $store
+   *   The store.
+   *
+   * @return \Drupal\Core\Entity\Query\QueryInterface
+   *   The entity query.
+   */
+  protected function buildLoadQuery(OrderTypeInterface $order_type, StoreInterface $store) {
+    $query = $this->getQuery();
+
+    $or_condition = $query->orConditionGroup()
+      ->condition('end_date', gmdate('Y-m-d'), '>=')
+      ->notExists('end_date', gmdate('Y-m-d'));
+    $query
+      ->condition('stores', [$store->id()], 'IN')
+      ->condition('order_types', [$order_type->id()], 'IN')
+      ->condition('start_date', gmdate('Y-m-d'), '<=')
+      ->condition('status', TRUE)
+      ->condition($or_condition);
+    return $query;
   }
 
 }
