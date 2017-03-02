7e0fc7d54e5f24016be89f8a25b5ff79714c698d
tavi toporjinschi <vasike@gmail.com>
2017-03-02 11:12:25 +0000

    Issue #2855838 by mglaman, vasike: Deleting a promotion does not delete its coupons

--- modules/promotion/commerce_promotion.post_update.php
+++ modules/promotion/commerce_promotion.post_update.php
@@ -14,3 +14,29 @@ function commerce_promotion_post_update_1() {
   $fields = commerce_promotion_entity_base_field_info($order_definition);
   $entity_definition_update->installFieldStorageDefinition('coupons', 'commerce_order', 'commerce_promotion', $fields['coupons']);
 }
+
+/**
+ * Delete orphaned coupons.
+ */
+function commerce_promotion_post_update_2() {
+  /** @var \Drupal\commerce_promotion\PromotionStorageInterface $promotion_storage */
+  $promotion_storage = \Drupal::service('entity_type.manager')->getStorage('commerce_promotion');
+  /** @var \Drupal\commerce_promotion\Entity\PromotionInterface[] $promotions */
+  $promotions = $promotion_storage->loadMultiple();
+  $coupons_ids = [];
+  foreach ($promotions as $promotion) {
+    foreach ($promotion->get('coupons') as $coupons_item) {
+      $coupons_ids[] = $coupons_item->target_id;
+    }
+  }
+
+  $delete_coupons_ids = \Drupal::entityQuery('commerce_promotion_coupon')
+    ->condition('id', $coupons_ids, 'NOT IN')
+    ->execute();
+
+  /** @var \Drupal\commerce_promotion\CouponStorageInterface $coupon_storage */
+  $coupon_storage = \Drupal::service('entity_type.manager')->getStorage('commerce_promotion_coupon');
+  /** @var \Drupal\commerce_promotion\Entity\CouponInterface[] $coupons */
+  $delete_coupons = $coupon_storage->loadMultiple($delete_coupons_ids);
+  $coupon_storage->delete($delete_coupons);
+}
