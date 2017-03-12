8f1e82977037238b31b970425932c5e459fad0e1
mglaman <mglaman@2416470.no-reply.drupal.org>
2017-03-12 23:23:23 +0100

    Issue #2833780 by mglaman, bojanz: Cancelled orders that are carts keep having products added to them

--- modules/cart/tests/src/Kernel/CartManagerTest.php
+++ modules/cart/tests/src/Kernel/CartManagerTest.php
@@ -17,6 +17,8 @@ use Drupal\Tests\commerce\Kernel\CommerceKernelTestBase;
  */
 class CartManagerTest extends CommerceKernelTestBase {
 
+  use CartManagerTestTrait;
+
   /**
    * The cart manager.
    *
@@ -94,24 +96,6 @@ class CartManagerTest extends CommerceKernelTestBase {
 
     $user = $this->createUser();
     $this->user = $this->reloadEntity($user);
-  }
-
-  /**
-   * Install commerce cart.
-   *
-   * Due to issues with hook_entity_bundle_create, we need to run this manually
-   * and cannot add commerce_cart to the $modules property.
-   *
-   * @see https://www.drupal.org/node/2711645
-   *
-   * @todo patch core so it doesn't explode in Kernel tests.
-   */
-  protected function installCommerceCart() {
-    $this->enableModules(['commerce_cart']);
-    $this->installConfig('commerce_cart');
-    $this->container->get('entity.definition_update_manager')->applyUpdates();
-    $this->cartProvider = $this->container->get('commerce_cart.cart_provider');
-    $this->cartManager = $this->container->get('commerce_cart.cart_manager');
   }
 
   /**
