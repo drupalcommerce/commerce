4ea46ac551ffe521c396d92a98c89b39b420c4bd
tavi toporjinschi <vasike@gmail.com>
2017-03-17 11:40:18 +0000

    Improve update script - PR request.

--- modules/payment/commerce_payment.post_update.php
+++ modules/payment/commerce_payment.post_update.php
@@ -11,43 +11,48 @@
  * @see https://www.drupal.org/node/2861181
  */
 function commerce_payment_post_update_1(&$sandbox) {
-  // Get the user ids that have 'commerce_remote_id' field not empty.
-  // First check is any user has this field set.
-  if (\Drupal::entityQuery('user')->exists('commerce_remote_id')->count()->execute() == 0) {
-    return t('No user Remote ID field data to update.');
-  }
-  $result = \Drupal::entityQuery('user')->exists('commerce_remote_id')->execute();
-
   // Use the sandbox to store the information needed to track progression.
   if (!isset($sandbox['current'])) {
-    // The count of entities visited so far.
-    $sandbox['current'] = 0;
-    // Total entities that must be visited.
-    $sandbox['max'] = count($result);
-    // A place to store messages during the run.
-  }
+    // First check is any user has this field set.
+    if ($count = \Drupal::entityQuery('user')->exists('commerce_remote_id')->count()->execute()) {
+      // The count of entities visited so far.
+      $sandbox['current'] = 0;
+      // Total entities that must be visited.
+      $sandbox['max'] = $count;
+      // A place to store messages during the run.
 
-  // Get the first payment gateway for every payment module.
-  $payment_gateways_by_module = [];
-  /** @var \Drupal\commerce_payment\PaymentGatewayStorageInterface $payment_gateway_storage */
-  $payment_gateway_storage = \Drupal::entityTypeManager()->getStorage('commerce_payment_gateway');
-  $payment_gateways = $payment_gateway_storage->loadMultiple();
-  uasort($payment_gateways, [$payment_gateway_storage->getEntityType()->getClass(), 'sort']);
-  foreach ($payment_gateways as $payment_gateway) {
-    $payment_gateway_data = $payment_gateway->toArray();
-    $module = reset($payment_gateway_data['dependencies']['module']);
-    if (!isset($payment_gateways_by_module[$module])) {
-      $payment_gateways_by_module[$module] = $payment_gateway;
+      // Get the first payment gateway for every payment module.
+      $payment_gateways_by_module = [];
+      /** @var \Drupal\commerce_payment\PaymentGatewayStorageInterface $payment_gateway_storage */
+      $payment_gateway_storage = \Drupal::entityTypeManager()->getStorage('commerce_payment_gateway');
+      $payment_gateways = $payment_gateway_storage->loadMultiple();
+      uasort($payment_gateways, [$payment_gateway_storage->getEntityType()->getClass(), 'sort']);
+      foreach ($payment_gateways as $payment_gateway) {
+        $payment_gateway_data = $payment_gateway->toArray();
+        $module = reset($payment_gateway_data['dependencies']['module']);
+        if (!isset($payment_gateways_by_module[$module])) {
+          $payment_gateways_by_module[$module] = $payment_gateway;
+        }
+      }
+      $sandbox['payment_gateways'] = $payment_gateways;
+      $sandbox['payment_gateways_by_module'] = $payment_gateways_by_module;
+      $sandbox['user_storage'] = \Drupal::entityTypeManager()->getStorage('user');
+    }
+    else {
+      return t('No user Remote ID field data to update.');
     }
   }
 
   // Process entities by groups of 20.
   $limit = 20;
-  $result = array_slice($result, $sandbox['current'], $limit);
-  $user_storage = \Drupal::entityTypeManager()->getStorage('user');
+  // Get the user ids that have 'commerce_remote_id' field not empty.
+  $result = \Drupal::entityQuery('user')
+    ->exists('commerce_remote_id')
+    ->range($sandbox['current'], $limit)
+    ->execute();
 
   foreach ($result as $uid) {
-    $user = $user_storage->load($uid);
+    $user = $sandbox['user_storage']->load($uid);
 
     $save = FALSE;
     /** @var \Drupal\commerce\Plugin\Field\FieldType\RemoteIdFieldItemListInterface $remote_ids */
@@ -55,11 +60,12 @@ function commerce_payment_post_update_1(&$sandbox) {
     /** @var \Drupal\commerce\Plugin\Field\FieldType\RemoteIdItem $item */
     foreach ($remote_ids as $index => $item) {
       $current_provider = $item->getValue()['provider'];
-      if (in_array($current_provider, array_keys($payment_gateways_by_module))) {
-        $item->set('provider', $payment_gateways_by_module[$current_provider]->id());
+      if (in_array($current_provider, array_keys($sandbox['payment_gateways_by_module']))) {
+        $payment_gateway = $sandbox['payment_gateways_by_module'][$current_provider];
+        $item->set('provider', $payment_gateway->id() . '-' . $payment_gateway->getPlugin()->getMode());
         $save = TRUE;
       }
-      elseif (!in_array($current_provider, array_keys($payment_gateways))) {
+      elseif (!in_array($current_provider, array_keys($sandbox['payment_gateways']))) {
         // Delete "orphaned" Remote Ids.
         $remote_ids->removeItem($index);
         $save = TRUE;
