caf23a5b6f8b35bf171815009fdc80037d4da825
Sean Wickham <swickham@acromediainc.com>
2017-04-07 01:24:43 -0700

    Issue #2863864: Added tests for loading of expired and maxed usaged promotions.

--- modules/promotion/tests/src/Kernel/PromotionStorageTest.php
+++ modules/promotion/tests/src/Kernel/PromotionStorageTest.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\Tests\commerce_promotion\Kernel;
 
+use Drupal\commerce_order\Entity\Order;
 use Drupal\commerce_order\Entity\OrderType;
+use Drupal\commerce_order\Entity\OrderItemType;
 use Drupal\commerce_promotion\Entity\Coupon;
 use Drupal\commerce_promotion\Entity\Promotion;
 use Drupal\Core\Datetime\DrupalDateTime;
@@ -21,6 +23,20 @@ class PromotionStorageTest extends CommerceKernelTestBase {
    * @var \Drupal\commerce_promotion\PromotionStorageInterface
    */
   protected $promotionStorage;
+
+  /**
+   * The usage.
+   *
+   * @var \Drupal\commerce_promotion\PromotionUsageInterface
+   */
+  protected $usage;
+
+  /**
+   * The test order.
+   *
+   * @var \Drupal\commerce_order\Entity\OrderInterface
+   */
+  protected $order;
 
   /**
    * Modules to enable.
@@ -55,6 +71,25 @@ class PromotionStorageTest extends CommerceKernelTestBase {
     $this->installSchema('commerce_promotion', ['commerce_promotion_usage']);
 
     $this->promotionStorage = $this->container->get('entity_type.manager')->getStorage('commerce_promotion');
+    $this->usage = $this->container->get('commerce_promotion.usage');
+
+    OrderItemType::create([
+      'id' => 'test',
+      'label' => 'Test',
+      'orderType' => 'default',
+    ])->save();
+
+    $this->order = Order::create([
+      'type' => 'default',
+      'state' => 'completed',
+      'mail' => 'test@example.com',
+      'ip_address' => '127.0.0.1',
+      'order_id' => '6',
+      'order_number' => '6',
+      'store_id' => $this->store,
+      'uid' => $this->createUser()->id(),
+      'order_items' => [],
+    ]);
   }
 
   /**
@@ -249,6 +284,60 @@ class PromotionStorageTest extends CommerceKernelTestBase {
     $promotion = array_shift($promotions);
     $this->assertEquals($promotion2->label(), $promotion->label());
     $promotion = array_shift($promotions);
+    $this->assertEquals($promotion1->label(), $promotion->label());
+  }
+
+  public function testLoadExpired() {
+    $valid_promotion = Promotion::create([
+      'name' => 'Valid Promotion',
+      'status' => TRUE,
+      'start_date' => gmdate('Y-m-d', time()),
+      'end_date' => gmdate('Y-m-d', time() + 86400),
+    ]);
+    $this->assertEquals(SAVED_NEW, $valid_promotion->save());
+
+    $expired_promotion = Promotion::create([
+      'name' => 'Expired Promotion',
+      'status' => TRUE,
+      'start_date' => gmdate('Y-m-d', time() - 172800),
+      'end_date' => gmdate('Y-m-d', time() - 86400),
+    ]);
+    $this->assertEquals(SAVED_NEW, $expired_promotion->save());
+
+    $promotions = $this->promotionStorage->loadExpired();
+    $this->assertEquals(sizeof($promotions), 1);
+
+    $promotion = reset($promotions);
+    $this->assertEquals($expired_promotion->label(), $promotion->label());
+  }
+
+  public function testLoadUsed() {
+    $promotion1 = Promotion::create([
+      'name' => 'Promotion 1',
+      'status' => TRUE,
+      'usage_limit' => 1,
+    ]);
+    $this->assertEquals(SAVED_NEW, $promotion1->save());
+    $this->usage->addUsage($this->order, $promotion1);
+
+    $promotion2 = Promotion::create([
+      'name' => 'Promotion 2',
+      'status' => TRUE,
+      'usage_limit' => 2,
+    ]);
+    $this->assertEquals(SAVED_NEW, $promotion2->save());
+    $this->usage->addUsage($this->order, $promotion2);
+
+    $promotion3 = Promotion::create([
+      'name' => 'Promotion 3',
+      'status' => TRUE,
+    ]);
+    $this->assertEquals(SAVED_NEW, $promotion3->save());
+
+    $promotions = $this->promotionStorage->loadMaxedUsage();
+    $this->assertEquals(sizeof($promotions), 1);
+
+    $promotion = reset($promotions);
     $this->assertEquals($promotion1->label(), $promotion->label());
   }
 
