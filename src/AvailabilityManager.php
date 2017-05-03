0beb9beb0082e9e694c141bac07ba6ee20acaeab
Steve Oliver <steve@circatree.com>
2017-05-03 09:26:04 -0700

    Use PHP_INT_MIN and PHP_INT_MAX for default availability response min and max.

--- src/AvailabilityManager.php
+++ src/AvailabilityManager.php
@@ -32,8 +32,8 @@ class AvailabilityManager implements AvailabilityManagerInterface {
    * {@inheritdoc}
    */
   public function check(PurchasableEntityInterface $entity, $quantity, Context $context) {
-    $min = .0000001;
-    $max = 9999999;
+    $min = PHP_INT_MIN;
+    $max = PHP_INT_MAX;
 
     $has_opinion = FALSE;
     foreach ($this->checkers as $checker) {
