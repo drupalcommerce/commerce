365e5bbd89ec64c753a9302481e073a7fd4b7cb6
Oleksandr Trotsenko <oleksandr.trotsenko@gmail.com>
2017-06-27 00:09:29 -0500

    Issue #2689919 by joshmiller, bucefal91: Allowing configuring the view for displaying order items.

--- modules/order/src/Plugin/Field/FieldFormatter/OrderItemTable.php
+++ modules/order/src/Plugin/Field/FieldFormatter/OrderItemTable.php
@@ -2,9 +2,14 @@
 
 namespace Drupal\commerce_order\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FormatterInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'commerce_order_item_table' formatter.
@@ -17,7 +22,53 @@ use Drupal\Core\Field\FormatterBase;
  *   },
  * )
  */
-class OrderItemTable extends FormatterBase {
+class OrderItemTable extends FormatterBase implements FormatterInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * @var EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new OrderItemTable object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings settings.
+   * @param EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager service.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new self(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -26,8 +77,7 @@ class OrderItemTable extends FormatterBase {
     $order = $items->getEntity();
     return [
       '#type' => 'view',
-      // @todo Allow the view to be configurable.
-      '#name' => 'commerce_order_item_table',
+      '#name' => $this->getSetting('view'),
       '#arguments' => [$order->id()],
       '#embed' => TRUE,
     ];
@@ -40,6 +90,48 @@ class OrderItemTable extends FormatterBase {
     $entity_type = $field_definition->getTargetEntityTypeId();
     $field_name = $field_definition->getName();
     return $entity_type == 'commerce_order' && $field_name == 'order_items';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'view' => 'commerce_order_item_table',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+
+    $view_storage = $this->entityTypeManager->getStorage('view');
+    $available_form_views = [];
+    foreach ($view_storage->loadMultiple() as $view) {
+      if ($view->get('base_table') == $this->entityTypeManager->getDefinition('commerce_order_item')->getBaseTable()) {
+        $available_form_views[$view->id()] = $view->label();
+      }
+    }
+
+    $form['view'] = [
+      '#type' => 'select',
+      '#title' => $this->t('View'),
+      '#options' => $available_form_views,
+      '#default_value' => $this->getSetting('view'),
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $view = $this->entityTypeManager->getStorage('view')->load($this->getSetting('view'));
+    return [$this->t('View: @view', array('@view' => $view->label()))];
   }
 
 }
