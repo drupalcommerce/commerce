e726090c502fe6531b5f6e2251d989b0f7ca8191
Fabien CLEMENT <fabien.clement@niji.fr>
2017-06-29 12:12:39 +0200

    Issue #2890670: WSOD creating checkout flow

--- modules/checkout/src/Plugin/Commerce/CheckoutPane/OrderSummary.php
+++ modules/checkout/src/Plugin/Commerce/CheckoutPane/OrderSummary.php
@@ -30,7 +30,9 @@ class OrderSummary extends CheckoutPaneBase implements CheckoutPaneInterface {
    */
   public function buildConfigurationSummary() {
     $view_storage = $this->entityTypeManager->getStorage('view');
-    $view = $view_storage->load($this->configuration['view']);
+    if (!$view = $view_storage->load($this->configuration['view'])) {
+      return '';
+    }
     $summary = $this->t('View: @view', ['@view' => $view->label()]);
 
     return $summary;
