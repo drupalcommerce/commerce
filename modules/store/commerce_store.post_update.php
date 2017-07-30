33e33c87a87ea05b7191f518aadbaba9e191fcd7
Bojan Zivanovic <bojanz@gmail.com>
2017-07-30 15:47:36 +0200

    Revert "Issue #2897929 by bojanz: Move Stores to admin/commerce/config"
    This reverts commit 7e3ccd97e1be5f72bdacf4c2729e3e00e0ede5d5.

--- modules/store/commerce_store.post_update.php
+++ modules/store/commerce_store.post_update.php
@@ -36,35 +36,3 @@ function commerce_store_post_update_1() {
 
   return $message;
 }
-
-/**
- * Revert the Stores view.
- */
-function commerce_store_post_update_2() {
-  /** @var \Drupal\commerce\Config\ConfigUpdaterInterface $config_updater */
-  $config_updater = \Drupal::service('commerce.config_updater');
-
-  $views = [
-    'views.view.commerce_stores',
-  ];
-  $result = $config_updater->revert($views, FALSE);
-
-  $success_results = $result->getSucceeded();
-  $failure_results = $result->getFailed();
-  $message = '';
-  if ($success_results) {
-    $message = t('Succeeded:') . '<br>';
-    foreach ($success_results as $success_message) {
-      $message .= $success_message . '<br>';
-    }
-    $message .= '<br>';
-  }
-  if ($failure_results) {
-    $message .= t('Failed:') . '<br>';
-    foreach ($failure_results as $failure_message) {
-      $message .= $failure_message . '<br>';
-    }
-  }
-
-  return $message;
-}
