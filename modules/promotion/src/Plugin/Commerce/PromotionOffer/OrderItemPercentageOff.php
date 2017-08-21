b715bd2372d1f379a98fb5f5f48a3a4678c83df3
Sean Wickham <swickham@acromediainc.com>
2017-08-20 23:49:40 -0700

    Apply existing order item adjustments when calculating percentage off promotion.

--- modules/promotion/src/Plugin/Commerce/PromotionOffer/OrderItemPercentageOff.php
+++ modules/promotion/src/Plugin/Commerce/PromotionOffer/OrderItemPercentageOff.php
@@ -4,6 +4,7 @@ namespace Drupal\commerce_promotion\Plugin\Commerce\PromotionOffer;
 
 use Drupal\commerce_order\Adjustment;
 use Drupal\commerce_promotion\Entity\PromotionInterface;
+use Drupal\commerce_price\Price;
 use Drupal\Core\Entity\EntityInterface;
 
 /**
@@ -24,17 +25,32 @@ class OrderItemPercentageOff extends PercentageOffBase {
     $this->assertEntity($entity);
     /** @var \Drupal\commerce_order\Entity\OrderItemInterface $order_item */
     $order_item = $entity;
-    $adjustment_amount = $order_item->getUnitPrice()->multiply($this->getPercentage());
+
+    $item_adjustments = $order_item->getAdjustments();
+    $item_adjustment_total = 0;
+
+    // Previous adjustments on the order item need to be included in
+    // calculations.
+    if (count($item_adjustments)) {
+      foreach ($item_adjustments as $item_adjustment) {
+        $item_adjustment_total += (float) $item_adjustment->getAmount()->getNumber();
+      }
+    }
+
+    $item_adjustment_amount = new Price((string) $item_adjustment_total, $item_adjustment->getAmount()->getCurrencyCode());
+    $adjustment_amount = $order_item->getUnitPrice()->add($item_adjustment_amount)->multiply($this->getPercentage());
     $adjustment_amount = $this->rounder->round($adjustment_amount);
 
-    $order_item->addAdjustment(new Adjustment([
-      'type' => 'promotion',
-      // @todo Change to label from UI when added in #2770731.
-      'label' => t('Discount'),
-      'amount' => $adjustment_amount->multiply('-1'),
-      'percentage' => $this->getPercentage(),
-      'source_id' => $promotion->id(),
-    ]));
+    if (!$adjustment_amount->isZero()) {
+      $order_item->addAdjustment(new Adjustment([
+        'type' => 'promotion',
+        // @todo Change to label from UI when added in #2770731.
+        'label' => t('Discount'),
+        'amount' => $adjustment_amount->multiply('-1'),
+        'percentage' => $this->getPercentage(),
+        'source_id' => $promotion->id(),
+      ]));
+    }
   }
 
 }
