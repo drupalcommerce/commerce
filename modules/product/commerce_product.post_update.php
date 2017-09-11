01c97abd621c516fd32083d3d5f38e7dda6b26e1
berdir <berdir@214652.no-reply.drupal.org>
2017-09-11 12:31:32 +0200

    Issue #2898258 by bojanz, Berdir: Sync the ProductForm UX with the 8.4.x NodeForm

--- modules/product/commerce_product.post_update.php
+++ modules/product/commerce_product.post_update.php
@@ -100,3 +100,35 @@ function commerce_product_post_update_3() {
 
   return $message;
 }
+
+/**
+ * Revert the default product form display.
+ */
+function commerce_product_post_update_4() {
+  /** @var \Drupal\commerce\Config\ConfigUpdaterInterface $config_updater */
+  $config_updater = \Drupal::service('commerce.config_updater');
+
+  $views = [
+    'core.entity_form_display.commerce_product.default.default',
+  ];
+  $result = $config_updater->revert($views, TRUE);
+
+  $success_results = $result->getSucceeded();
+  $failure_results = $result->getFailed();
+  $message = '';
+  if ($success_results) {
+    $message = t('Succeeded:') . '<br>';
+    foreach ($success_results as $success_message) {
+      $message .= $success_message . '<br>';
+    }
+    $message .= '<br>';
+  }
+  if ($failure_results) {
+    $message .= t('Failed:') . '<br>';
+    foreach ($failure_results as $failure_message) {
+      $message .= $failure_message . '<br>';
+    }
+  }
+
+  return $message;
+}
