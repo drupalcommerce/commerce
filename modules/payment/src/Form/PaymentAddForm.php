48b9f4d4c998017eda0e0acf463a8eee0fbe3188
Steve Oliver <steve@circatree.com>
2017-11-20 21:56:01 -0800

    Update PaymentAddForm.php

--- modules/payment/src/Form/PaymentAddForm.php
+++ modules/payment/src/Form/PaymentAddForm.php
@@ -141,7 +141,7 @@ class PaymentAddForm extends FormBase implements ContainerInjectionInterface {
       /** @var \Drupal\commerce_payment\PaymentMethodStorageInterface $payment_method_storage */
       $payment_method_storage = $this->entityTypeManager->getStorage('commerce_payment_method');
       $billing_countries = $this->order->getStore()->getBillingCountries();
-      $payment_methods = $payment_method_storage->loadReusable($this->order->getCustomer(), $selected_payment_gateway, $billing_countries);
+      $payment_methods = $payment_method_storage->loadStored($this->order, $selected_payment_gateway, $billing_countries);
       if (!empty($payment_methods)) {
         $selected_payment_method = reset($payment_methods);
         $payment_method_options = [];
