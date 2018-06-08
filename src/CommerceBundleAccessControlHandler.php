65345a8df1a677ee67e847ef96a6ce2116b435ee
lisastreeter <lisastreeter@3439677.no-reply.drupal.org>
2018-06-08 12:06:04 +0200

    Issue #2949235 by lisastreeter, bojanz: Bundle entity types (product type, order type, etc) need to support the "view label" access check

--- src/CommerceBundleAccessControlHandler.php
+++ src/CommerceBundleAccessControlHandler.php
@@ -3,30 +3,25 @@
 namespace Drupal\commerce;
 
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Entity\EntityAccessControlHandler as CoreEntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\entity\BundleEntityAccessControlHandler;
 
 /**
  * Defines the access control handler for bundles.
  */
-class CommerceBundleAccessControlHandler extends CoreEntityAccessControlHandler {
+class CommerceBundleAccessControlHandler extends BundleEntityAccessControlHandler {
 
   /**
    * {@inheritdoc}
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
-    /** @var \Drupal\commerce\Entity\CommerceBundleEntityInterface $entity */
-    $admin_permission = $entity->getEntityType()->getAdminPermission();
-    if ($operation === 'delete') {
-      if ($entity->isLocked()) {
-        return AccessResult::forbidden()->addCacheableDependency($entity);
-      }
-      else {
-        return AccessResult::allowedIfHasPermission($account, $admin_permission)->addCacheableDependency($entity);
-      }
+    if ($operation === 'delete' && $entity->isLocked()) {
+      return AccessResult::forbidden()->addCacheableDependency($entity);
     }
-    return AccessResult::allowedIfHasPermission($account, $admin_permission);
+    else {
+      return parent::checkAccess($entity, $operation, $account);
+    }
   }
 
 }
