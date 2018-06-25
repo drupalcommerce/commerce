9f40b30acb40eb1343b1d4e7b7c632c330622c75
Bojan Zivanovic <bojanz@gmail.com>
2018-06-25 21:31:57 +0200

    Issue #2981526 by bojanz: Percentage order offers should make sure that the per-item discounts add up to the expected subtotal one

--- modules/promotion/src/Plugin/Commerce/PromotionOffer/OrderPercentageOff.php
+++ modules/promotion/src/Plugin/Commerce/PromotionOffer/OrderPercentageOff.php
@@ -3,11 +3,16 @@
 namespace Drupal\commerce_promotion\Plugin\Commerce\PromotionOffer;
 
 use Drupal\commerce_order\Adjustment;
+use Drupal\commerce_order\PriceSplitterInterface;
+use Drupal\commerce_price\RounderInterface;
 use Drupal\commerce_promotion\Entity\PromotionInterface;
 use Drupal\Core\Entity\EntityInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides the percentage off offer for orders.
+ *
+ * The discount is split between order items, to simplify VAT taxes and refunds.
  *
  * @CommercePromotionOffer(
  *   id = "order_percentage_off",
@@ -18,25 +23,69 @@ use Drupal\Core\Entity\EntityInterface;
 class OrderPercentageOff extends PercentageOffBase {
 
   /**
+   * The price splitter.
+   *
+   * @var \Drupal\commerce_order\PriceSplitterInterface
+   */
+  protected $splitter;
+
+  /**
+   * Constructs a new OrderPercentageOff object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The pluginId for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\commerce_price\RounderInterface $rounder
+   *   The rounder.
+   * @param \Drupal\commerce_order\PriceSplitterInterface $splitter
+   *   The splitter.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RounderInterface $rounder, PriceSplitterInterface $splitter) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $rounder);
+
+    $this->splitter = $splitter;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('commerce_price.rounder'),
+      $container->get('commerce_order.price_splitter')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function apply(EntityInterface $entity, PromotionInterface $promotion) {
     $this->assertEntity($entity);
     /** @var \Drupal\commerce_order\Entity\OrderInterface $order */
     $order = $entity;
-    // Reduce each individual order item, to simplify VAT taxes, refunds.
-    foreach ($order->getItems() as $order_item) {
-      $adjustment_amount = $order_item->getTotalPrice()->multiply($this->getPercentage());
-      $adjustment_amount = $this->rounder->round($adjustment_amount);
+    $percentage = $this->getPercentage();
+    // Calculate the order-level discount and split it across order items.
+    $amount = $order->getSubtotalPrice()->multiply($percentage);
+    $amount = $this->rounder->round($amount);
+    $amounts = $this->splitter->split($order, $amount, $percentage);
 
-      $order_item->addAdjustment(new Adjustment([
-        'type' => 'promotion',
-        // @todo Change to label from UI when added in #2770731.
-        'label' => t('Discount'),
-        'amount' => $adjustment_amount->multiply('-1'),
-        'percentage' => $this->getPercentage(),
-        'source_id' => $promotion->id(),
-      ]));
+    foreach ($order->getItems() as $order_item) {
+      if (isset($amounts[$order_item->id()])) {
+        $order_item->addAdjustment(new Adjustment([
+          'type' => 'promotion',
+          // @todo Change to label from UI when added in #2770731.
+          'label' => t('Discount'),
+          'amount' => $amounts[$order_item->id()]->multiply('-1'),
+          'percentage' => $percentage,
+          'source_id' => $promotion->id(),
+        ]));
+      }
     }
   }
 
