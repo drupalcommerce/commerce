ff07d9d078a8b779fa92d4f7c11a92a03e61a22e
Bojan Zivanovic <bojanz@gmail.com>
2018-08-22 14:26:17 +0200

    Issue #2947227 by GoZ, bojanz: Rework CouponAccessControlHandler

--- modules/promotion/src/CouponAccessControlHandler.php
+++ modules/promotion/src/CouponAccessControlHandler.php
@@ -8,7 +8,15 @@ use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
 
 /**
- * Controls coupon access based on the parent promotion.
+ * Provides an access control handler for coupons.
+ *
+ * Coupons are always managed in the scope of their parent (the promotion),
+ * so they have a simplified permission set, and rely on parent access:
+ * - A coupon can be viewed if the parent promotion can be viewed.
+ * - A coupon can be updated or deleted if the parent promotion can be updated.
+ * - A coupon can be created if the user has permission to update promotions.
+ *
+ * The "administer commerce_promotion" permission is also respected.
  */
 class CouponAccessControlHandler extends EntityAccessControlHandler {
 
@@ -16,13 +24,18 @@ class CouponAccessControlHandler extends EntityAccessControlHandler {
    * {@inheritdoc}
    */
   public function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
-    /** @var \Drupal\Core\Access\AccessResult $result */
-    $result = parent::checkAccess($entity, $operation, $account);
-
-    if ($result->isNeutral()) {
-      /** @var \Drupal\commerce_promotion\Entity\CouponInterface $entity */
-      $result = $entity->getPromotion()->access('update', $account, TRUE);
+    if ($account->hasPermission($this->entityType->getAdminPermission())) {
+      return AccessResult::allowed()->cachePerPermissions();
     }
+
+    /** @var \Drupal\commerce_promotion\Entity\CouponInterface $entity */
+    $promotion = $entity->getPromotion();
+    if (!$promotion) {
+      // The coupon is malformed.
+      return AccessResult::forbidden()->addCacheableDependency($entity);
+    }
+    $parent_operation = ($operation == 'view') ? 'view' : 'update';
+    $result = $promotion->access($parent_operation, $account, TRUE);
 
     return $result;
   }
