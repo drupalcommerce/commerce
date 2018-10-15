0f78ee4e74f3105345a72ec0aa8c6df3765f7338
Bojan Zivanovic <bojanz@gmail.com>
2018-10-15 14:59:51 +0200

    Issue #3005098 by rlmumford, bojanz: Backwards compatibility break in DefaultPriceResolver (due to list_price support)

--- modules/price/src/Resolver/DefaultPriceResolver.php
+++ modules/price/src/Resolver/DefaultPriceResolver.php
@@ -15,7 +15,12 @@ class DefaultPriceResolver implements PriceResolverInterface {
    */
   public function resolve(PurchasableEntityInterface $entity, $quantity, Context $context) {
     $field_name = $context->getData('field_name', 'price');
-    if ($entity->hasField($field_name) && !$entity->get($field_name)->isEmpty()) {
+    if ($field_name == 'price') {
+      // Use the price getter to allow custom purchasable entity types to have
+      // computed prices that are not backed by a field called "price".
+      return $entity->getPrice();
+    }
+    elseif ($entity->hasField($field_name) && !$entity->get($field_name)->isEmpty()) {
       return $entity->get($field_name)->first()->toPrice();
     }
   }
