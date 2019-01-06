9ecd04b6ed09fbcc4523d8129f02742db76f4a5a
bojanz <bojanz@86106.no-reply.drupal.org>
2019-01-06 22:05:19 +0100

    Issue #3024128 by bojanz: EntitySelectWidgetTest should use its own reference field for testing

--- tests/src/Functional/EntitySelectWidgetTest.php
+++ tests/src/Functional/EntitySelectWidgetTest.php
@@ -3,9 +3,9 @@
 namespace Drupal\Tests\commerce\Functional;
 
 use Drupal\commerce\EntityHelper;
-use Drupal\commerce_product\Entity\Product;
 use Drupal\field\Entity\FieldStorageConfig;
-use Drupal\Core\Entity\Entity\EntityFormDisplay;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\node\Entity\Node;
 
 /**
  * Tests the Entity select widget.
@@ -20,7 +20,8 @@ class EntitySelectWidgetTest extends CommerceBrowserTestBase {
    * @var array
    */
   public static $modules = [
-    'commerce_product',
+    'node',
+    'datetime',
   ];
 
   /**
@@ -38,18 +39,18 @@ class EntitySelectWidgetTest extends CommerceBrowserTestBase {
   protected $referenceField;
 
   /**
-   * The test product.
+   * The test node.
    *
-   * @var \Drupal\commerce_product\Entity\ProductInterface
+   * @var \Drupal\node\NodeInterface
    */
-  protected $product;
+  protected $node;
 
   /**
    * {@inheritdoc}
    */
   protected function getAdministratorPermissions() {
     return array_merge([
-      'administer commerce_product',
+      'bypass node access',
     ], parent::getAdministratorPermissions());
   }
 
@@ -59,8 +60,26 @@ class EntitySelectWidgetTest extends CommerceBrowserTestBase {
   protected function setUp() {
     parent::setUp();
 
-    $this->referenceField = FieldStorageConfig::loadByName('commerce_product', 'stores');
-    $display = EntityFormDisplay::load('commerce_product.default.default');
+    $this->drupalCreateContentType(['type' => 'article', 'name' => 'Article']);
+
+    $field_storage = FieldStorageConfig::create([
+      'field_name' => 'stores',
+      'entity_type' => 'node',
+      'type' => 'entity_reference',
+      'settings' => [
+        'target_type' => 'commerce_store',
+      ],
+      'cardinality' => FieldStorageConfig::CARDINALITY_UNLIMITED,
+    ]);
+    $field_storage->save();
+    $field = FieldConfig::create([
+      'field_storage' => $field_storage,
+      'label' => 'Stores',
+      'bundle' => 'article',
+      'required' => TRUE,
+    ]);
+    $field->save();
+    $display = commerce_get_entity_display('node', 'article', 'form');
     $display->setComponent('stores', [
       'type' => 'commerce_entity_select',
       'settings' => [
@@ -68,14 +87,10 @@ class EntitySelectWidgetTest extends CommerceBrowserTestBase {
       ],
     ])->save();
 
-    $variation = $this->createEntity('commerce_product_variation', [
-      'type' => 'default',
-      'sku' => strtolower($this->randomMachineName()),
-    ]);
-    $this->product = $this->createEntity('commerce_product', [
-      'type' => 'default',
+    $this->referenceField = $field_storage;
+    $this->node = $this->createEntity('node', [
+      'type' => 'article',
       'title' => $this->randomMachineName(),
-      'variations' => [$variation],
     ]);
     // Set the first store.
     $this->stores[] = $this->store;
@@ -85,17 +100,16 @@ class EntitySelectWidgetTest extends CommerceBrowserTestBase {
    * Tests widget's hidden input type.
    */
   public function testWidget() {
-    $form_url = 'product/' . $this->product->id() . '/edit';
     // Since the field is required, the widget should be a hidden element.
     $store_id = $this->stores[0]->id();
-    $this->drupalGet($form_url);
+    $this->drupalGet($this->node->toUrl('edit-form'));
     $field = $this->getSession()->getPage()->find('xpath', '//input[@type="hidden" and @name="stores[target_id][value]" and @value="' . $store_id . '"]');
     $this->assertNotEmpty($field);
 
     // Create another store. The widget should now be a set of checkboxes.
     $this->createStores(1);
     $store_ids = EntityHelper::extractIds($this->stores);
-    $this->drupalGet($form_url);
+    $this->drupalGet($this->node->toUrl('edit-form'));
     $this->assertNotNull($this->getSession()->getPage()->find('xpath', '//input[@type="checkbox" and starts-with(@name,"stores")]'));
     $this->assertSession()->checkboxNotChecked('edit-stores-target-id-value-1');
     $this->assertSession()->checkboxNotChecked('edit-stores-target-id-value-2');
@@ -103,17 +117,21 @@ class EntitySelectWidgetTest extends CommerceBrowserTestBase {
     $edit['stores[target_id][value][' . $store_ids[0] . ']'] = $store_ids[0];
     $edit['stores[target_id][value][' . $store_ids[1] . ']'] = FALSE;
     $this->submitForm($edit, t('Save'));
-    $this->assertSession()->statusCodeEquals(200);
-    \Drupal::entityTypeManager()->getStorage('commerce_product')->resetCache();
-    $this->product = Product::load($this->product->id());
-    $this->assertFieldValues($this->product->getStoreIds(), [$store_ids[0]], 'The correct store has been set on the product.');
-    $this->drupalGet($form_url);
+
+    \Drupal::entityTypeManager()->getStorage('node')->resetCache();
+    $this->node = Node::load($this->node->id());
+    $node_store_ids = [];
+    foreach ($this->node->get('stores') as $store_item) {
+      $node_store_ids[] = $store_item->target_id;
+    }
+    $this->assertFieldValues($node_store_ids, [$store_ids[0]]);
+    $this->drupalGet($this->node->toUrl('edit-form'));
     $this->assertSession()->checkboxChecked('edit-stores-target-id-value-' . $store_ids[0]);
     $this->assertSession()->checkboxNotChecked('edit-stores-target-id-value-' . $store_ids[1]);
 
     // Reduce the cardinality to 1. Checkboxes should now be radios.
     $this->referenceField->setCardinality(1)->save();
-    $this->drupalGet($form_url);
+    $this->drupalGet($this->node->toUrl('edit-form'));
     $this->assertNotNull($this->getSession()->getPage()->find('xpath', '//input[@type="radio" and @name="stores[target_id][value]"]'));
     $this->assertSession()->checkboxChecked('edit-stores-target-id-value-' . $store_ids[0]);
     $this->assertSession()->checkboxNotChecked('edit-stores-target-id-value-' . $store_ids[1]);
@@ -124,18 +142,21 @@ class EntitySelectWidgetTest extends CommerceBrowserTestBase {
       return $store->label() . ' (' . $store->id() . ')';
     }, $this->stores);
     $this->referenceField->setCardinality(FieldStorageConfig::CARDINALITY_UNLIMITED)->save();
-    $this->drupalGet($form_url);
+    $this->drupalGet($this->node->toUrl('edit-form'));
     $this->assertNotNull($this->getSession()->getPage()->find('xpath', '//input[@id="edit-stores-target-id-value" and starts-with(@class, "form-autocomplete")]'));
     $this->assertSession()->fieldValueEquals('stores[target_id][value]', $store_labels[0]);
     // Reference both stores 1 and 2.
     $edit = [];
     $edit['stores[target_id][value]'] = $store_labels[0] . ', ' . $store_labels[1];
     $this->submitForm($edit, t('Save'));
-    $this->assertSession()->statusCodeEquals(200);
-    \Drupal::entityTypeManager()->getStorage('commerce_product')->resetCache();
-    $this->product = Product::load($this->product->id());
-    $this->assertFieldValues($this->product->getStoreIds(), [$store_ids[0], $store_ids[1]], 'The correct stores have been set on the product.');
-    $this->drupalGet($form_url);
+    \Drupal::entityTypeManager()->getStorage('node')->resetCache();
+    $this->node = Node::load($this->node->id());
+    $node_store_ids = [];
+    foreach ($this->node->get('stores') as $store_item) {
+      $node_store_ids[] = $store_item->target_id;
+    }
+    $this->assertFieldValues($node_store_ids, [$store_ids[0], $store_ids[1]]);
+    $this->drupalGet($this->node->toUrl('edit-form'));
     $this->assertSession()->fieldValueEquals('stores[target_id][value]', $store_labels[0] . ', ' . $store_labels[1]);
   }
 
