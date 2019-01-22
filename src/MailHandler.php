55f409d6d0b0e667d00adabdf092d55d0d7b15ca
Bojan Zivanovic <bojanz@gmail.com>
2019-01-22 13:53:40 +0100

    Issue #3017080 followup by bojanz, Berdir: Simplify the MailHandler service

--- src/MailHandler.php
+++ src/MailHandler.php
@@ -5,11 +5,7 @@ namespace Drupal\commerce;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Mail\MailManagerInterface;
-use Drupal\Core\Render\RenderContext;
-use Drupal\Core\Render\Renderer;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\Core\Theme\ThemeInitializationInterface;
-use Drupal\Core\Theme\ThemeManagerInterface;
 use Drupal\user\UserInterface;
 
 class MailHandler implements MailHandlerInterface {
@@ -38,27 +34,6 @@ class MailHandler implements MailHandlerInterface {
   protected $mailManager;
 
   /**
-   * The renderer.
-   *
-   * @var \Drupal\Core\Render\RendererInterface
-   */
-  protected $renderer;
-
-  /**
-   * The theme manager.
-   *
-   * @var \Drupal\Core\Theme\ThemeManagerInterface
-   */
-  protected $themeManager;
-
-  /**
-   * The theme initialization.
-   *
-   * @var \Drupal\Core\Theme\ThemeInitializationInterface
-   */
-  protected $themeInitialization;
-
-  /**
    * Constructs a new MailHandler object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
@@ -67,20 +42,11 @@ class MailHandler implements MailHandlerInterface {
    *   The language manager.
    * @param \Drupal\Core\Mail\MailManagerInterface $mail_manager
    *   The mail manager.
-   * @param \Drupal\Core\Render\Renderer $renderer
-   *   The renderer.
-   * @param \Drupal\Core\Theme\ThemeManagerInterface $theme_manager
-   *   The theme manager.
-   * @param \Drupal\Core\Theme\ThemeInitializationInterface $theme_initialization
-   *   The theme initialization.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, LanguageManagerInterface $language_manager, MailManagerInterface $mail_manager, Renderer $renderer, ThemeManagerInterface $theme_manager, ThemeInitializationInterface $theme_initialization) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, LanguageManagerInterface $language_manager, MailManagerInterface $mail_manager) {
     $this->storeStorage = $entity_type_manager->getStorage('commerce_store');
     $this->languageManager = $language_manager;
     $this->mailManager = $mail_manager;
-    $this->renderer = $renderer;
-    $this->themeManager = $theme_manager;
-    $this->themeInitialization = $theme_initialization;
   }
 
   /**
@@ -111,37 +77,15 @@ class MailHandler implements MailHandlerInterface {
       'id' => 'mail',
       'from' => $default_store->getEmail(),
       'subject' => $subject,
+      // The body will be rendered in commerce_mail(), because that's what
+      // MailManager expects. The correct theme and render context aren't
+      // setup until then.
+      'body' => $body,
     ];
     if (isset($params['bcc'])) {
       $default_params['headers']['Bcc'] = $params['bcc'];
     }
     $params = array_replace($default_params, $params);
-
-    // Switch the theme to the configured mail theme.
-    $mail_theme = NULL;
-    $current_active_theme = $this->themeManager->getActiveTheme();
-
-    // The Mail System module swaps out core's MailManager, adding a
-    // getMailTheme() method. However, this method is not on any interface.
-    if (method_exists($this->mailManager, 'getMailTheme')) {
-      $mail_theme = $this->mailManager->getMailTheme();
-      if ($mail_theme && $mail_theme != $current_active_theme->getName()) {
-        $initialized_mail_theme = $this->themeInitialization->initTheme($mail_theme);
-        $this->themeManager->setActiveTheme($initialized_mail_theme);
-      }
-    }
-
-    try {
-      $params['body'] = $this->renderer->executeInRenderContext(new RenderContext(), function () use ($body) {
-        return $this->renderer->render($body);
-      });
-    }
-    finally {
-      // Revert the active theme.
-      if ($mail_theme != $current_active_theme->getName()) {
-        $this->themeManager->setActiveTheme($current_active_theme);
-      }
-    }
 
     // Replicated logic from EmailAction and contact's MailHandler.
     if ($account->isAuthenticated()) {
