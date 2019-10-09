ef0125f3af929aeedf076ec6fe9c598e2301bf8c
Bojan Zivanovic <bojanz@gmail.com>
2019-10-09 22:30:34 +0200

    Issue #3086907 by bojanz: Add an "admin" view mode for profiles

--- modules/order/commerce_order.post_update.php
+++ modules/order/commerce_order.post_update.php
@@ -250,3 +250,35 @@ function commerce_order_post_update_10() {
     $profile_type->save();
   }
 }
+
+/**
+ * Add the "admin" view mode to profiles.
+ */
+function commerce_order_post_update_11() {
+  /** @var \Drupal\commerce\Config\ConfigUpdaterInterface $config_updater */
+  $config_updater = \Drupal::service('commerce.config_updater');
+
+  $config_names = [
+    'core.entity_view_mode.profile.admin',
+    'core.entity_view_display.profile.customer.admin',
+  ];
+  $result = $config_updater->import($config_names);
+
+  $success_results = $result->getSucceeded();
+  $failure_results = $result->getFailed();
+  if ($success_results) {
+    $message = t('Succeeded:') . '<br>';
+    foreach ($success_results as $success_message) {
+      $message .= $success_message . '<br>';
+    }
+    $message .= '<br>';
+  }
+  if ($failure_results) {
+    $message .= t('Failed:') . '<br>';
+    foreach ($failure_results as $failure_message) {
+      $message .= $failure_message . '<br>';
+    }
+  }
+
+  return $message;
+}
