a7bbd23dcf82cf73e37f25b171c3192b3256241e
Bojan Zivanovic <bojanz@gmail.com>
2019-10-11 12:26:38 +0200

    Issue #3087350 by bojanz: The product variation kernel test is missing coverage

--- modules/product/tests/src/Kernel/Entity/ProductVariationTest.php
+++ modules/product/tests/src/Kernel/Entity/ProductVariationTest.php
@@ -3,6 +3,8 @@
 namespace Drupal\Tests\commerce_product\Kernel\Entity;
 
 use Drupal\commerce_price\Price;
+use Drupal\commerce_product\Entity\ProductAttribute;
+use Drupal\commerce_product\Entity\ProductAttributeValue;
 use Drupal\commerce_product\Entity\ProductVariation;
 use Drupal\commerce_product\Entity\Product;
 use Drupal\Tests\commerce\Kernel\CommerceKernelTestBase;
@@ -41,6 +43,7 @@ class ProductVariationTest extends CommerceKernelTestBase {
 
     $this->installEntitySchema('commerce_product_variation');
     $this->installEntitySchema('commerce_product');
+    $this->installEntitySchema('commerce_product_attribute_value');
     $this->installConfig(['commerce_product']);
 
     $user = $this->createUser();
@@ -56,6 +59,8 @@ class ProductVariationTest extends CommerceKernelTestBase {
    * @covers ::setSku
    * @covers ::getTitle
    * @covers ::setTitle
+   * @covers ::getListPrice
+   * @covers ::setListPrice
    * @covers ::getPrice
    * @covers ::setPrice
    * @covers ::isActive
@@ -99,6 +104,10 @@ class ProductVariationTest extends CommerceKernelTestBase {
     $variation->setTitle('My title');
     $this->assertEquals('My title', $variation->getTitle());
 
+    $list_price = new Price('19.99', 'USD');
+    $variation->setListPrice($list_price);
+    $this->assertEquals($list_price, $variation->getListPrice());
+
     $price = new Price('9.99', 'USD');
     $variation->setPrice($price);
     $this->assertEquals($price, $variation->getPrice());
@@ -124,6 +133,99 @@ class ProductVariationTest extends CommerceKernelTestBase {
     $variation->delete();
     $product = $this->reloadEntity($product);
     $this->assertFalse($product->hasVariation($variation));
+
+    // Confirm that the attribute methods return nothing by default.
+    $this->assertEmpty($variation->getAttributeFieldNames());
+    $this->assertEmpty($variation->getAttributeValueIds());
+    $this->assertEmpty($variation->getAttributeValues());
+  }
+
+  /**
+   * @covers ::getAttributeFieldNames
+   * @covers ::getAttributeValueIds
+   * @covers ::getAttributeValueId
+   * @covers ::getAttributeValues
+   * @covers ::getAttributeValue
+   */
+  public function testAttributes() {
+    $color_attribute = ProductAttribute::create([
+      'id' => 'color',
+      'label' => 'Color',
+    ]);
+    $color_attribute->save();
+
+    $size_attribute = ProductAttribute::create([
+      'id' => 'size',
+      'label' => 'Size',
+    ]);
+    $size_attribute->save();
+
+    $attribute_field_manager = $this->container->get('commerce_product.attribute_field_manager');
+    $attribute_field_manager->createField($color_attribute, 'default');
+    $attribute_field_manager->createField($size_attribute, 'default');
+
+    $color_attribute_value = ProductAttributeValue::create([
+      'attribute' => 'color',
+      'name' => 'Blue',
+      'weight' => 0,
+    ]);
+    $color_attribute_value->save();
+    $color_attribute_value = $this->reloadEntity($color_attribute_value);
+
+    $size_attribute_value = ProductAttributeValue::create([
+      'attribute' => 'size',
+      'name' => 'Medium',
+      'weight' => 0,
+    ]);
+    $size_attribute_value->save();
+    $size_attribute_value = $this->reloadEntity($size_attribute_value);
+
+    $product = Product::create([
+      'type' => 'default',
+      'title' => 'My Product Title',
+    ]);
+    $product->save();
+    $product = $this->reloadEntity($product);
+
+    /** @var \Drupal\commerce_product\Entity\ProductVariationInterface $variation */
+    $variation = ProductVariation::create([
+      'type' => 'default',
+      'product_id' => $product->id(),
+      'attribute_color' => $color_attribute_value->id(),
+      'attribute_size' => $size_attribute_value->id(),
+    ]);
+    $variation->save();
+    $variation = $this->reloadEntity($variation);
+
+    $this->assertEquals(['attribute_color', 'attribute_size'], $variation->getAttributeFieldNames());
+    $this->assertEquals([
+      'attribute_color' => $color_attribute_value->id(),
+      'attribute_size' => $size_attribute_value->id(),
+    ], $variation->getAttributeValueIds());
+    $this->assertEquals($color_attribute_value->id(), $variation->getAttributeValueId('attribute_color'));
+    $this->assertEquals($size_attribute_value->id(), $variation->getAttributeValueId('attribute_size'));
+
+    $this->assertEquals([
+      'attribute_color' => $color_attribute_value,
+      'attribute_size' => $size_attribute_value,
+    ], $variation->getAttributeValues());
+    $this->assertEquals($color_attribute_value, $variation->getAttributeValue('attribute_color'));
+    $this->assertEquals($size_attribute_value, $variation->getAttributeValue('attribute_size'));
+
+    // Confirm that empty fields are excluded properly.
+    $variation->set('attribute_size', NULL);
+    $variation->save();
+    $variation = $this->reloadEntity($variation);
+
+    $this->assertEquals([
+      'attribute_color' => $color_attribute_value->id(),
+    ], $variation->getAttributeValueIds());
+    $this->assertNull($variation->getAttributeValueId('attribute_size'));
+
+    $this->assertEquals([
+      'attribute_color' => $color_attribute_value,
+    ], $variation->getAttributeValues());
+    $this->assertNull($variation->getAttributeValue('attribute_size'));
   }
 
 }
