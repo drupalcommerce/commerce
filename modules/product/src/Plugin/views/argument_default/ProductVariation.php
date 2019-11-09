8f071be4d266da3314f5b06d6f1edbfa27296e1f
jsacksick <jsacksick@972218.no-reply.drupal.org>
2019-11-09 16:38:39 +0100

    Issue #3093082 by jsacksick: Create a views argument default plugin to extract a Product variation ID from URL

---
+++ modules/product/src/Plugin/views/argument_default/ProductVariation.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\commerce_product\Plugin\views\argument_default;
+
+use Drupal\commerce_product\Entity\ProductVariationInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Cache\CacheableDependencyInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Default argument plugin to extract a product variation.
+ *
+ * @ViewsArgumentDefault(
+ *   id = "product_variation",
+ *   title = @Translation("Product variation ID from URL")
+ * )
+ */
+class ProductVariation extends ArgumentDefaultPluginBase implements CacheableDependencyInterface {
+
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a new ProductVariation instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getArgument() {
+    if (($variation = $this->routeMatch->getParameter('commerce_product_variation')) && $variation instanceof ProductVariationInterface) {
+      return $variation->id();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return Cache::PERMANENT;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return ['url'];
+  }
+
+}
