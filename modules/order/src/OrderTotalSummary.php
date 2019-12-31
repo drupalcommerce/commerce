2a06a7cea75b105550e9566af47de94e1525f7f7
Bojan Zivanovic <bojanz@gmail.com>
2019-12-31 10:29:47 +0100

    Revert "Issue #2893860 by czigor, bojanz: Provide a "subtotal without tax" to the order total template"
    This reverts commit c5f74852de1802955d080df5dbda88a670506623.

--- modules/order/src/OrderTotalSummary.php
+++ modules/order/src/OrderTotalSummary.php
@@ -29,13 +29,6 @@ class OrderTotalSummary implements OrderTotalSummaryInterface {
   public function buildTotals(OrderInterface $order) {
     $adjustments = $order->collectAdjustments();
     $adjustments = $this->adjustmentTransformer->processAdjustments($adjustments);
-    $subtotal = $order->getSubtotalPrice();
-    $subtotal_without_tax = $subtotal;
-    foreach ($adjustments as $adjustment) {
-      if ($adjustment->getType() === 'tax' && $adjustment->isIncluded()) {
-        $subtotal_without_tax = $subtotal_without_tax->subtract($adjustment->getAmount());
-      }
-    }
     // Convert the adjustments to arrays.
     $adjustments = array_map(function (Adjustment $adjustment) {
       return $adjustment->toArray();
@@ -46,8 +39,7 @@ class OrderTotalSummary implements OrderTotalSummaryInterface {
     }
 
     return [
-      'subtotal' => $subtotal,
-      'subtotal_without_tax' => $subtotal_without_tax,
+      'subtotal' => $order->getSubtotalPrice(),
       'adjustments' => $adjustments,
       'total' => $order->getTotalPrice(),
     ];
