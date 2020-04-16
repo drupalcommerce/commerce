84f825c40796f155eb2991fd062383f09d4324bf
Jonathan Sacksick <jonathan.sacksick@gmail.com>
2020-04-16 11:06:26 +0300

    Issue #3127569 by jsacksick: Performance improvements to AvailabilityOrderProcessor.

--- modules/order/src/AvailabilityOrderProcessor.php
+++ modules/order/src/AvailabilityOrderProcessor.php
@@ -5,6 +5,7 @@ namespace Drupal\commerce_order;
 use Drupal\commerce\AvailabilityManagerInterface;
 use Drupal\commerce\Context;
 use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Provides an order processor that removes entities that are no longer available.
@@ -19,13 +20,23 @@ class AvailabilityOrderProcessor implements OrderProcessorInterface {
   protected $availabilityManager;
 
   /**
+   * The order item storage.
+   *
+   * @var \Drupal\commerce_order\OrderItemStorageInterface
+   */
+  protected $orderItemStorage;
+
+  /**
    * Constructs a new AvailabilityOrderProcessor object.
    *
    * @param \Drupal\commerce\AvailabilityManagerInterface $availability_manager
    *   The availability manager.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
    */
-  public function __construct(AvailabilityManagerInterface $availability_manager) {
+  public function __construct(AvailabilityManagerInterface $availability_manager, EntityTypeManagerInterface $entity_type_manager) {
     $this->availabilityManager = $availability_manager;
+    $this->orderItemStorage = $entity_type_manager->getStorage('commerce_order_item');
   }
 
   /**
@@ -34,16 +45,32 @@ class AvailabilityOrderProcessor implements OrderProcessorInterface {
   public function process(OrderInterface $order) {
     // @todo Get $context as an argument to process().
     $context = new Context($order->getCustomer(), $order->getStore());
+    $order_items_to_remove = [];
     foreach ($order->getItems() as $order_item) {
       $purchased_entity = $order_item->getPurchasedEntity();
-      if ($purchased_entity) {
-        $available = $this->availabilityManager->check($purchased_entity, $order_item->getQuantity(), $context);
-        if (!$available) {
-          $order->removeItem($order_item);
-          $order_item->delete();
-        }
+      if (!$purchased_entity) {
+        continue;
+      }
+      $available = $this->availabilityManager->check($purchased_entity, $order_item->getQuantity(), $context);
+      if (!$available) {
+        // We collect the order item ids to remove here instead of directly
+        // calling $order->removeItem(), mostly for performance reasons since
+        // that allows us to remove multiple order items at a time and
+        // recalculate the order total only once.
+        $order_items_to_remove[$order_item->id()] = $order_item;
       }
     }
+    if (!$order_items_to_remove) {
+      return;
+    }
+    $order_item_ids = array_keys($order_items_to_remove);
+    $order->get('order_items')->filter(function($item) use ($order_item_ids) {
+      return !in_array($item->target_id, $order_item_ids);
+    });
+    $this->orderItemStorage->delete($order_items_to_remove);
+    // Since we don't call removeItem(), we manually have to recalculate the
+    // order total.
+    $order->recalculateTotalPrice();
   }
 
 }
