55434c4bd74e70b9493125d453aa7002ab58a61b
mglaman <mglaman@2416470.no-reply.drupal.org>
2020-05-15 21:00:17 -0500

    Issue #3137116 by mglaman, zaporylie: Move createPayment to SupportsStoredPaymentMethodsInterface

--- modules/payment/src/Plugin/Commerce/PaymentGateway/SupportsStoredPaymentMethodsInterface.php
+++ modules/payment/src/Plugin/Commerce/PaymentGateway/SupportsStoredPaymentMethodsInterface.php
@@ -2,12 +2,29 @@
 
 namespace Drupal\commerce_payment\Plugin\Commerce\PaymentGateway;
 
+use Drupal\commerce_payment\Entity\PaymentInterface;
 use Drupal\commerce_payment\Entity\PaymentMethodInterface;
 
 /**
  * Defines the interface for gateways which support storing payment methods.
  */
 interface SupportsStoredPaymentMethodsInterface {
+
+  /**
+   * Creates a payment.
+   *
+   * @param \Drupal\commerce_payment\Entity\PaymentInterface $payment
+   *   The payment.
+   * @param bool $capture
+   *   Whether the created payment should be captured (VS authorized only).
+   *   Allowed to be FALSE only if the plugin supports authorizations.
+   *
+   * @throws \InvalidArgumentException
+   *   If $capture is FALSE but the plugin does not support authorizations.
+   * @throws \Drupal\commerce_payment\Exception\PaymentGatewayException
+   *   Thrown when the transaction fails for any reason.
+   */
+  public function createPayment(PaymentInterface $payment, $capture = TRUE);
 
   /**
    * Creates a payment method with the given payment details.
